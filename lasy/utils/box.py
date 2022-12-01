c17b06b2f8c263760d92eb2fa0a2e4ca3a39dfe6
Maxence Thevenet <maxence.thevenet@desy.de>
2022-12-01 21:39:48 +0100

    add a few methods to Box class

--- lasy/utils/box.py
+++ lasy/utils/box.py
@@ -35,3 +35,31 @@ class Box:
         for i in range(self.ndims):
             self.axes.append(np.linspace(lo[i], hi[i], npoints[i]))
             self.dx.append(self.axes[i][1] - self.axes[i][0])
+
+    def position_to_indices(position):
+        """
+        From a position in physical space, returns position in index space.
+
+        Parameters
+        ----------
+        position : tuple of scalars
+            Position to be converted to index space. 2 elements for rz, 3 for xyz
+        """
+        indices = []
+        for i in range(self.ndims):
+            indices.append((position[i]-self.lo[i])/self.dx[i])
+        return indices
+
+    def indices_to_position(indices):
+        """
+        From a position in index space, returns position in physical space.
+
+        Parameters
+        ----------
+        indices : tuple of scalars
+            Position in index space to be converted to physical space. 2 elements for rz, 3 for xyz
+        """
+        positions = []
+        for i in range(self.ndims):
+            positions.append(box.lo[i] + box.dx[i]*positions[i])
+        return positions
