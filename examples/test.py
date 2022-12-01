41ee39820e57f67492e56e1c6d81e8db39abba04
Maxence Thevenet <maxence.thevenet@desy.de>
2022-12-01 18:41:31 +0100

    add example

---
+++ examples/test.py
@@ -0,0 +1,15 @@
+import numpy as np
+from lasy.laser import Laser
+
+lo = (1,2,3)
+hi = (4,6,8)
+dim = 'xyz'
+npoints=(2,3,2)
+
+array_in = np.zeros(npoints)
+wavelength=.8e-6
+pol = (1,0)
+laser = Laser(dim, lo, hi, array_in, wavelength, pol)
+laser.write_to_file()
+laser.propagate(1)
+laser.write_to_file()
