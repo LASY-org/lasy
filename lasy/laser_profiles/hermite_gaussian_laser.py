d70db7a30f9776aa6c612a6ffad2afc108c6c4b9
Remi Lehe <remi.lehe@normalesup.org>
2022-12-08 14:03:37 -0800

    Avoid normalizing the energy twice

--- lasy/laser_profiles/hermite_gaussian_laser.py
+++ lasy/laser_profiles/hermite_gaussian_laser.py
@@ -17,12 +17,12 @@ class HermiteGaussianLaser(LaserProfile):
         .. math::
             E_u(\boldsymbol{x}_\perp,t) = Re\left[ E_0\,
             H_{n_x}\left ( \frac{\sqrt{2} x}{w_0}\right )\,
-            H_{n_y}\left ( \frac{\sqrt{2} y}{w_0}\right )\,            
+            H_{n_y}\left ( \frac{\sqrt{2} y}{w_0}\right )\,
             \exp\left( -\frac{\boldsymbol{x}_\perp^2}{w_0^2}
             - \frac{(t-t_{peak})^2}{\tau^2} -i\omega_0(t-t_{peak})
             + i\phi_{cep}\right) \times p_u \right]
         where :math:`u` is either :math:`x` or :math:`y`, :math:`H_{n}` is the
-        Hermite polynomial of order :math:`n`, :math:`p_u` is the polarization 
+        Hermite polynomial of order :math:`n`, :math:`p_u` is the polarization
         vector, :math:`Re` represent the real part, and
         :math:`\boldsymbol{x}_\perp` is the transverse coordinate (orthogonal
         to the propagation direction). The other parameters in this formula
@@ -65,8 +65,8 @@ class HermiteGaussianLaser(LaserProfile):
         super().__init__(wavelength, pol)
         self.laser_energy = laser_energy
         self.w0 = w0
-        self.n_x = n_x 
-        self.n_y = n_y 
+        self.n_x = n_x
+        self.n_y = n_y
         self.tau = tau
         self.t_peak = t_peak
         self.cep_phase = cep_phase
@@ -74,7 +74,7 @@ class HermiteGaussianLaser(LaserProfile):
     def evaluate( self, envelope, box ):
         """
         Fills the envelope field of the laser
-        
+
         Parameters
         -----------
         envelope: ndarrays (V/m)
@@ -98,21 +98,16 @@ class HermiteGaussianLaser(LaserProfile):
         elif box.dim == 'rt':
             r = box.axes[0]
             """
-            For these modes to make sense in rt, we need to 
-            have multiple modes and likely represent as a 
-            series of Laguerre Gauss Modes for accurate 
+            For these modes to make sense in rt, we need to
+            have multiple modes and likely represent as a
+            series of Laguerre Gauss Modes for accurate
             reresentation. Until then, we pass a simple gaussian
             and a warning
             """
             assert ((self.n_x ==0) and (self.n_y == 0 )),"Modes for \
                 n_x > 0 or n_y > 0 are not yet implemented in dimension rt"
-        
+
             transverse_profile = np.exp( -r**2/self.w0**2 )
-            
+
             envelope[0,:,:] = transverse_profile[:,np.newaxis] * \
                             long_profile[np.newaxis, :]
-
-        # Normalize to the correct energy
-        current_energy = compute_laser_energy(envelope, box)
-        norm_factor = (self.laser_energy/current_energy)**.5
-        envelope *= norm_factor
