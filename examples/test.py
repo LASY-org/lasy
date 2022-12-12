98492798460df45256e7f29246ad244a276b50aa
Peter Blum <peter.blum@desy.de>
2022-12-12 14:29:18 +0100

    added openpmdexample to test.py

--- examples/test.py
+++ examples/test.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from openpmd_viewer import OpenPMDTimeSeries
 from lasy.laser import Laser
 from lasy.profiles.gaussian_profile import GaussianProfile
 
@@ -33,3 +35,40 @@ laser = Laser(dim, lo, hi, npoints, profile)
 laser.write_to_file('laserRZ')
 laser.propagate(1)
 laser.write_to_file('laserRZ')
+
+
+# load in the data with openPMD-viewer
+ts_3d = OpenPMDTimeSeries('../examples/')
+ts_circ = OpenPMDTimeSeries('../examples/')
+
+# plot some data
+# #3d-geometry
+# Slice across y (i.e. in a plane parallel to x-z)
+plt.figure()
+Ez_real1, info_Ez_real1 = ts_3d.get_field(field='E_real', coord='z', iteration=0, slice_across='t', plot=True)
+plt.savefig('3dfield_Ez_real.png')
+
+# Slice across t (i.e. in a plane parallel to x-z)
+plt.figure()
+Ey_imag1, info_Ey_imag1 = ts_3d.get_field(field='E_imag', coord='y', iteration=0, slice_across='t', plot=True)
+plt.savefig('3dfield_Ey_imag.png')
+
+# Get the full 3D Cartesian array
+Ez_3d, info_Ez_3d = ts_3d.get_field(field='E_real', coord='z', iteration=0, slice_across=None)
+print(Ez_3d.ndim)
+# currently only 2 dimensions as there is only one time entry
+
+# #RZ-geometry
+# Slice across  (i.e. in a plane parallel to x-z)
+plt.figure()
+Ey2, info_Ey2 = ts_circ.get_field(field='E_real', coord='r', iteration=0, m='all', slice_across='t', plot=True)
+plt.savefig('RZfield_Ey.png')
+
+plt.figure()
+Er, info_Er = ts_circ.get_field(field='E_real', coord='r', iteration=0, m=0, slice_across='t', plot=True)
+plt.savefig('RZfield_Er.png')
+
+# Get the full 3D radial array
+Ey_3d, info_Ey3d = ts_circ.get_field(field='E_real', coord='y', iteration=0)
+print(Ey_3d.ndim)
+# currently only 2 dimensions as there is only one time entry
