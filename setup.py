8d3e2e999b1c78a02ae26571eaa07dcf3f8c3c9b
Axel Huebl <axel.huebl@plasma.ninja>
2023-01-05 16:47:48 +0100

    CI: Modernization (#62)

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import find_packages, setup
 
+# Get the package requirements from the requirements.txt file
+install_requires = []
+with open("./requirements.txt") as f:
+    install_requires = [line.strip("\n") for line in f.readlines()]
+
 setup(
     name='lasy',
     packages=find_packages('.'),
     description='LAser pulse manipulation made eaSY',
-    install_requires=['openpmd-api', 'scipy', 'numpy'],
+    install_requires=install_requires,
     tests_require=["pytest"],
 )
