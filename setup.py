0f820f55ee35068516aed775e2f24c59b098efed
Axel Huebl <axel.huebl@plasma.ninja>
2023-01-06 14:08:59 +0100

    Tests: openPMD-viewer (#70)

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import find_packages, setup
 
-# Get the package requirements from the requirements.txt file
+# Get the package requirements from the requirements.txt files
 install_requires = []
 with open("./requirements.txt") as f:
     install_requires = [line.strip("\n") for line in f.readlines()]
+tests_require = []
+with open("./tests/requirements.txt") as f:
+    tests_require = [line.strip("\n") for line in f.readlines()]
 
 setup(
     name='lasy',
     packages=find_packages('.'),
     description='LAser pulse manipulation made eaSY',
     install_requires=install_requires,
-    tests_require=["pytest"],
+    tests_require=tests_require,
+    extras_require = {
+        'tests': tests_require,
+    },
 )
