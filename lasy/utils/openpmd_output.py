23f2b97e4c35f08a112f66e522236d9594108b76
Remi Lehe <remi.lehe@normalesup.org>
2023-01-23 12:06:07 -0800

    Update openPMD output to conform to new standard

--- lasy/utils/openpmd_output.py
+++ lasy/utils/openpmd_output.py
@@ -1,5 +1,6 @@
 import numpy as np
 import openpmd_api as io
+from scipy.constants import c
 
 def write_to_openpmd_file(dim, file_prefix, file_format, grid,
                           wavelength, pol):
@@ -41,69 +42,51 @@ def write_to_openpmd_file(dim, file_prefix, file_format, grid,
     i = series.iterations[0]
 
     # Store metadata needed to reconstruct the field
-    i.set_attribute("wavelength", wavelength)
+    i.set_attribute("angularFrequency", 2*np.pi*c/wavelength)
     i.set_attribute("pol", pol)
 
-    # Define E_real, E_imag as scalar records
-    for comp_name in ['E_real', 'E_imag']:
+    # Define the mesh
+    m = i.meshes['laser_envelope']
+    m.grid_spacing = [ (hi-lo)/npoints for hi, lo, npoints in \
+                           zip( box.hi, box.lo, box.npoints ) ]
+    m.grid_global_offset = box.lo
+    m.unit_dimension = {
+        io.Unit_Dimension.M:  1,
+        io.Unit_Dimension.L:  1,
+        io.Unit_Dimension.I: -1,
+        io.Unit_Dimension.T: -3
+    }
+    if dim == 'xyt':
+        m.geometry = io.Geometry.cartesian
+        m.axis_labels = ['t', 'x', 'y']
+    elif dim == 'rt':
+        m.geometry = io.Geometry.thetaMode
+        m.axis_labels = ['t', 'r']
 
-        # Define the mesh
-        m = i.meshes[comp_name]
-        m.grid_spacing = [ (hi-lo)/npoints for hi, lo, npoints in \
-                               zip( box.hi, box.lo, box.npoints ) ]
-        m.grid_global_offset = box.lo
-        m.unit_dimension = {
-            io.Unit_Dimension.M:  1,
-            io.Unit_Dimension.L:  1,
-            io.Unit_Dimension.I: -1,
-            io.Unit_Dimension.T: -3
-        }
-        if dim == 'xyt':
-            m.geometry = io.Geometry.cartesian
-            m.axis_labels = ['t', 'x', 'y']
-        elif dim == 'rt':
-            m.geometry = io.Geometry.thetaMode
-            m.axis_labels = ['t', 'r']
+    # Define the dataset
+    dataset = io.Dataset(array.dtype, array.shape)
+    env = m[io.Mesh_Record_Component.SCALAR]
+    env.position = [0]*len(dim)
+    env.reset_dataset(dataset)
 
-        # Define the dataset
-        dataset = io.Dataset(array.real.dtype, array.real.shape)
-        E = m[io.Mesh_Record_Component.SCALAR]
-        E.position = [0]*len(dim)
-        E.reset_dataset(dataset)
+    # Pick the correct field
+    if dim == 'xyt':
+        data = array
 
-        # Pick the correct field
-        if dim == 'xyt':
-            if comp_name == 'E_real':
-                data = array.real.copy()
-            elif comp_name == 'E_imag':
-                data = array.imag.copy()
+    elif dim == 'rt':
+        # The representation of modes in openPMD
+        # (see https://github.com/openPMD/openPMD-standard/blob/latest/STANDARD.md#required-attributes-for-each-mesh-record)
+        # is different than the representation of modes internal to lasy.
+        # Thus, there is a non-trivial conversion here
+        ncomp = 2*box.n_azimuthal_modes - 1
+        data = np.zeros( (ncomp, box.npoints[0], box.npoints[1]), dtype=array.dtype )
+        data[0,:,:] = array[0,:,:]
+        for mode in range(1,box.n_azimuthal_modes):
+            # cos(m*theta) part of the mode
+            data[2*mode-1,:,:] = array[mode,:,:] + array[-mode,:,:]
+            # sin(m*theta) part of the mode
+            data[2*mode,:,:] = -1.j*array[mode,:,:] + 1.j*array[-mode,:,:]
 
-        elif dim == 'rt':
-            # The representation of modes in openPMD
-            # (see https://github.com/openPMD/openPMD-standard/blob/latest/STANDARD.md#required-attributes-for-each-mesh-record)
-            # is different than the representation of modes internal to lasy.
-            # Thus, there is a non-trivial conversion here
-            ncomp = 2*box.n_azimuthal_modes - 1
-            data = np.zeros( (ncomp, box.npoints[0], box.npoints[1]) )
-            if comp_name == 'E_real':
-                data[0,:,:] = array[0,:,:].real
-                for mode in range(1,box.n_azimuthal_modes):
-                    # Real part of the mode
-                    data[2*mode-1,:,:] = array[mode,:,:].real \
-                                            + array[-mode,:,:].real
-                    # Imaginary part of the mode
-                    data[2*mode,:,:] = array[mode,:,:].imag \
-                                            - array[-mode,:,:].imag
-            elif comp_name == 'E_imag':
-                data[0,:,:] = array[0,:,:].imag
-                for mode in range(1,box.n_azimuthal_modes):
-                    # Real part of the mode
-                    data[2*mode-1,:,:] = array[mode,:,:].imag \
-                                            + array[-mode,:,:].imag
-                    # Imaginary part of the mode
-                    data[2*mode,:,:] = -array[mode,:,:].real \
-                                            + array[-mode,:,:].real
-
-        E.store_chunk( data )
+    env.store_chunk( data )
 
     series.flush()
