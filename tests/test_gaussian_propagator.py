b7394623875ef31c0d6384306342803e9e150033
Igor Andriyash <igor.andriyash@gmail.com>
2023-02-10 11:45:49 +0100

    added a propagation test for the gaussian profile

---
+++ tests/test_gaussian_propagator.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+import numpy as np
+from lasy.laser import Laser
+from lasy.profiles.gaussian_profile import GaussianProfile
+
+@pytest.fixture(scope="function")
+def gaussian():
+    # Cases with Gaussian laser
+    wavelength=.8e-6
+    pol = (1,0)
+    laser_energy = 1. # J
+    t_peak = 0.e-15   # s
+    tau = 30.e-15     # s
+    w0 = 5.e-6        # m
+    profile = GaussianProfile(wavelength, pol, laser_energy, w0, tau, t_peak)
+
+    return profile
+
+def get_w0(laser):
+    if laser.dim == 'xyt':
+        Nt, Nx, Ny  = laser.field.field.shape
+        A2 = (np.abs(laser.field.field[:, :, Nx//2-1])**2).sum(0)
+        ax = laser.box.axes[1]
+    else:
+        A2 = (np.abs(laser.field.field[0])**2).sum(0)
+        ax = laser.box.axes[-1]
+        A2 = np.r_[A2[::-1][:-1], A2]
+        ax = np.r_[-ax[::-1][:-1], ax]
+
+    sigma = 2 * np.sqrt(np.average(ax**2, weights=A2) )
+
+    return sigma
+
+def check_gaussian_propagation(laser, propagation_distance=100e-6,
+                                propagation_step=10e-6):
+
+    w0 = laser.profile.trans_profile.w0
+    L_R = np.pi * w0**2 / laser.profile.lambda0
+
+    propagated_distance = 0.0
+    while propagated_distance <= propagation_distance:
+        propagated_distance += propagation_step
+        laser.propagate(propagation_step,)
+        w0_num = get_w0(laser)
+        w0_theor = w0 * np.sqrt( 1 + (propagated_distance / L_R)**2 )
+        err = 2 * np.abs(w0_theor - w0_num) / (w0_theor + w0_num)
+        assert(err < 1e-3)
+
+    return 0
+
+def test_3D_case(gaussian):
+    dim = 'xyt'
+    lo = (-25e-6, -25e-6, -60e-15)
+    hi = (+25e-6, +25e-6, +60e-15)
+    npoints = (100, 100, 100)
+
+    laser = Laser(dim, lo, hi, npoints, gaussian)
+    check_gaussian_propagation(laser)
+
+def test_RT_case(gaussian):
+    dim = 'rt'
+    lo = (0e-6, -60e-15)
+    hi = (25e-6, +60e-15)
+    npoints = (100, 100)
+
+    laser = Laser(dim, lo, hi, npoints, gaussian)
+    check_gaussian_propagation(laser)
