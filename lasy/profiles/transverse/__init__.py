65387dd1dcfaadd7705652b7bce095c508be1663
Maxence Thevenet <maxence.thevenet@desy.de>
2023-02-10 12:46:12 +0100

    fix a few scratches here and there

--- lasy/profiles/transverse/__init__.py
+++ lasy/profiles/transverse/__init__.py
@@ -1,7 +1,7 @@
 from .gaussian_profile import GaussianTransverseProfile
 from .hermite_gaussian_profile import HermiteGaussianTransverseProfile
 from .laguerre_gaussian_profile import LaguerreGaussianTransverseProfile
-from .super_gaussian_profile import SuperGaussianTransverProfile
+from .super_gaussian_profile import SuperGaussianTransverseProfile
 
 __all__ = [
     "GaussianTransverseProfile",
