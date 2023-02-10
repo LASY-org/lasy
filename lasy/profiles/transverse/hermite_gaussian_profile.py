8851952cc7cee34bc9c0162c871d106134213d0f
Soeren Jalas <soeren.jalas@desy.de>
2023-02-10 14:44:38 +0100

    Move addition feature to Laguerre and Hermite Gaussian profiles only

--- lasy/profiles/transverse/hermite_gaussian_profile.py
+++ lasy/profiles/transverse/hermite_gaussian_profile.py
@@ -63,3 +63,80 @@ class HermiteGaussianTransverseProfile(TransverseProfile):
         )
 
         return envelope
+
+    def __add__(self, other):
+        """Overload the + operations for laser profiles."""
+        return SummedTransverseProfile(self, other)
+
+    def __mul__(self, other):
+        """Overload the * operations for laser profiles."""
+        return ScaledTransverseProfile(self, other)
+
+    def __rmul__(self, other):
+        """Overload the * operations for laser profiles."""
+        return ScaledTransverseProfile(self, other)
+
+class SummedTransverseProfile(HermiteGaussianTransverseProfile):
+    """Class for a transverse profile that is the sum of several profiles."""
+
+    def __init__(self, *profiles):
+        """
+        Initialize the transverse profile.
+
+        Parameters
+        ----------
+        *profiles: list of HermiteGaussianTransverseProfile objects
+            The profiles to be summed.
+        """
+        # Store the input profiles
+        self.profiles = profiles
+        # Get the waist values from each profile
+        self.w0 = [p.w0 for p in self.profiles]
+        # Check if all waist values are equal
+        if all([w0 == self.w0[0] for w0 in self.w0]):
+            # If all waist values are equal, store the first one as the waist
+            self.w0 = self.w0[0]
+        else:
+            # If all waist values are not equal, raise a ValueError
+            raise ValueError("All profiles must have the same waist.")
+        # Unset p and m of the input profiles
+        self.n_x = None
+        self.n_y = None
+        # Check if all profiles are instances of HermiteGaussianTransverseProfile
+        if not all([isinstance(p, HermiteGaussianTransverseProfile) for p in self.profiles]):
+            # If not, raise a ValueError
+            raise ValueError("All profiles must be HermiteGaussianTransverseProfile objects.")
+
+
+    def _evaluate(self, x, y):
+        """Return the sum of the profiles."""
+        return sum([p.evaluate(x, y) for p in self.profiles])
+
+
+class ScaledTransverseProfile(HermiteGaussianTransverseProfile):
+    """Class for a transverse profile that is scaled by a factor."""
+
+    def __init__(self, profile, factor):
+        """
+        Initialize the transverse profile.
+
+        Parameters
+        ----------
+        profile: HermiteGaussianTransverseProfile object
+            The profile to be scaled.
+        factor: float
+            The scaling factor.
+        """
+        self.w0 = profile.w0
+        self.n_x = profile.n_x
+        self.n_y = profile.n_y
+        self.profile = profile
+        self.factor = factor
+        if not isinstance(self.factor, (int, float)):
+            raise ValueError("The scaling factor must be a float.")
+
+    def _evaluate(self, x, y):
+        """Return the scaled profile."""
+        return self.factor * self.profile.evaluate(x, y)
+
+
--- lasy/profiles/transverse/laguerre_gaussian_profile.py
+++ lasy/profiles/transverse/laguerre_gaussian_profile.py
@@ -45,7 +45,7 @@ class LaguerreGaussianTransverseProfile(TransverseProfile):
 
     def evaluate(self, x, y):
         """
-        Returns the transverse envelope
+        Returns the transverse envelope.
 
         Parameters
         ----------
@@ -73,3 +73,79 @@ class LaguerreGaussianTransverseProfile(TransverseProfile):
         )
 
         return envelope
+
+    def __add__(self, other):
+        """Overload the + operations for laser profiles."""
+        return SummedTransverseProfile(self, other)
+
+    def __mul__(self, other):
+        """Overload the * operations for laser profiles."""
+        return ScaledTransverseProfile(self, other)
+
+    def __rmul__(self, other):
+        """Overload the * operations for laser profiles."""
+        return ScaledTransverseProfile(self, other)
+
+class SummedTransverseProfile(LaguerreGaussianTransverseProfile):
+    """Class for a transverse profile that is the sum of several profiles."""
+
+    def __init__(self, *profiles):
+        """
+        Initialize the transverse profile.
+
+        Parameters
+        ----------
+        *profiles: list of LaguerreGaussianTransverseProfile objects
+            The profiles to be summed.
+        """
+        # Store the input profiles
+        self.profiles = profiles
+        # Get the waist values from each profile
+        self.w0 = [p.w0 for p in self.profiles]
+        # Check if all waist values are equal
+        if all([w0 == self.w0[0] for w0 in self.w0]):
+            # If all waist values are equal, store the first one as the waist
+            self.w0 = self.w0[0]
+        else:
+            # If all waist values are not equal, raise a ValueError
+            raise ValueError("All profiles must have the same waist.")
+        # Unset p and m of the input profiles
+        self.p = None
+        self.m = None
+        # Check if all profiles are instances of LaguerreGaussianTransverseProfile
+        if not all([isinstance(p, LaguerreGaussianTransverseProfile) for p in self.profiles]):
+            # If not, raise a ValueError
+            raise ValueError("All profiles must be LaguerreGaussian objects.")
+
+
+    def _evaluate(self, x, y):
+        """Return the sum of the profiles."""
+        return sum([p.evaluate(x, y) for p in self.profiles])
+
+
+class ScaledTransverseProfile(LaguerreGaussianTransverseProfile):
+    """Class for a transverse profile that is scaled by a factor."""
+
+    def __init__(self, profile, factor):
+        """
+        Initialize the transverse profile.
+
+        Parameters
+        ----------
+        profile: LaguerreGaussianTransverseProfile object
+            The profile to be scaled.
+        factor: float
+            The scaling factor.
+        """
+        self.w0 = profile.w0
+        self.p = profile.p
+        self.m = profile.m
+        self.profile = profile
+        self.factor = factor
+        if not isinstance(self.factor, (int, float)):
+            raise ValueError("The scaling factor must be a float.")
+
+    def _evaluate(self, x, y):
+        """Return the scaled profile."""
+        return self.factor * self.profile.evaluate(x, y)
+
