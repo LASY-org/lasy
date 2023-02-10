24b7b71186affc804626e4f993f0936dc38c6754
Remi Lehe <remi.lehe@normalesup.org>
2023-02-10 08:21:25 -0800

    Avoid storing omega and field_fft

--- lasy/laser.py
+++ lasy/laser.py
@@ -131,16 +131,15 @@ class Laser:
                 ]
 
         # Transform the field from temporal to frequency domain
-        self.field.field_fft = np.fft.fft(
+        field_fft = np.fft.fft(
             self.field.field, axis=time_axis_indx, norm="forward"
         )
 
-        # Create the frequency axis if necessary
-        if not hasattr(self.field, "omega"):
-            dt = self.box.dx[time_axis_indx]
-            omega0 = self.profile.omega0
-            Nt = self.field.field.shape[time_axis_indx]
-            self.field.omega = 2 * np.pi * np.fft.fftfreq(Nt, dt) + omega0
+        # Create the frequency axis
+        dt = self.box.dx[time_axis_indx]
+        omega0 = self.profile.omega0
+        Nt = self.field.field.shape[time_axis_indx]
+        omega = 2 * np.pi * np.fft.fftfreq(Nt, dt) + omega0
 
         if self.dim == "rt":
             # make 3D shape for the frequency axis
@@ -153,7 +152,7 @@ class Laser:
                     self.prop.append(
                         PropagatorResampling(
                             *spatial_axes,
-                            self.field.omega / scc.c,
+                            omega / scc.c,
                             mode=m,
                             backend=backend,
                             verbose=False,
@@ -161,9 +160,9 @@ class Laser:
                     )
             # Propagate the spectral image
             for i_m in range(self.box.azimuthal_modes.size):
-                transform_data = np.transpose(self.field.field_fft[i_m]).copy()
+                transform_data = np.transpose(field_fft[i_m]).copy()
                 self.prop[i_m].step(transform_data, distance, overwrite=True)
-                self.field.field_fft[i_m, :, :] = np.transpose(transform_data).copy()
+                field_fft[i_m, :, :] = np.transpose(transform_data).copy()
         else:
             # make 3D shape for the frequency axis
             omega_shape = (1, 1, self.field.field.shape[time_axis_indx])
@@ -175,14 +174,14 @@ class Laser:
                 spatial_axes = ((Lx, Nx), (Ly, Ny))
                 self.prop = PropagatorFFT2(
                     *spatial_axes,
-                    self.field.omega / scc.c,
+                    omega / scc.c,
                     backend=backend,
                     verbose=False,
                 )
             # Propagate the spectral image
-            transform_data = np.transpose(self.field.field_fft).copy()
+            transform_data = np.transpose(field_fft).copy()
             self.prop.step(transform_data, distance, overwrite=True)
-            self.field.field_fft[:, :, :] = np.transpose(transform_data).copy()
+            field_fft[:, :, :] = np.transpose(transform_data).copy()
 
         # Choose the time translation assuming propagation at v=c
         translate_time = distance / scc.c
@@ -192,13 +191,13 @@ class Laser:
         self.box.axes[time_axis_indx] += translate_time
 
         # Translate the phase of spectral image
-        self.field.field_fft *= np.exp(
-            -1j * translate_time * self.field.omega.reshape(omega_shape)
+        field_fft *= np.exp(
+            -1j * translate_time * omega.reshape(omega_shape)
         )
 
         # Transform field from frequency to temporal domain
-        self.field.field = np.fft.ifft(
-            self.field.field_fft, axis=time_axis_indx, norm="forward"
+        self.field.field[:,:,:] = np.fft.ifft(
+            field_fft, axis=time_axis_indx, norm="forward"
         )
 
         # Translate phase of the retrieved envelope by the distance
