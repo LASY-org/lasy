610049f76c05a315623fe92a8708e0a89adf999f
Axel Huebl <axel.huebl@plasma.ninja>
2023-04-17 09:47:42 -0700

    Fix: exp_data_utils.py

--- lasy/utils/exp_data_utils.py
+++ lasy/utils/exp_data_utils.py
@@ -3,15 +3,15 @@ import numpy as np
 
 def find_center_of_mass(img):
     """
-    Finds the center of mass of an image.
+    Find the center of mass of an image.
 
-    Parameters:
-    -----------
+    Parameters
+    ----------
     img: 2Darray of floats
         The image on which to calculate the COM
 
-    Returns:
-    --------
+    Return
+    ------
     x0 , y0: floats
         The center of mass of the image along the horizontal
         and the vertical. The units are in pixels.
@@ -31,8 +31,7 @@ def find_center_of_mass(img):
 
 def find_d4sigma(img):
     """
-    Finds the D4Sigma measurement of the spot size
-    in x and y in pixels.
+    Find the D4Sigma measurement of the spot size in x and y in pixels.
 
     https://en.wikipedia.org/wiki/Beam_diameter#D4%CF%83_or_second-moment_width
 
@@ -40,12 +39,11 @@ def find_d4sigma(img):
     ----------
     img : A numpy array containing the spatial intensity profile of a laser pulse.
 
-    Returns
-    -------
+    Return
+    ------
     D4sigX : The D4sigma along the first (x) axis
     D4sigY : The D4sigma along the second (y) axis
     """
-
     rows, cols = np.shape(img)
     x = np.linspace(0, cols - 1, cols)
     y = np.linspace(0, rows - 1, rows)
