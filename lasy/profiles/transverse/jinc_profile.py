51567ed2ce9de6defbf17cf7e03765f140e2193a
rob.shalloo <rob.shalloo@desy.de>
2023-04-25 22:15:46 +0200

    added new documentation for transverse profile from data and fixed jinc profile docs

--- lasy/profiles/transverse/jinc_profile.py
+++ lasy/profiles/transverse/jinc_profile.py
@@ -12,6 +12,7 @@ class JincTransverseProfile(TransverseProfile):
     .. math::
 
         \\mathcal{T}(x, y) = 2\\frac{J_1(r/w_0)}{r/w_0} \\textrm{, with } r=\\sqrt{x^2+y^2}
+    
     where :math:`J_1` is the Bessel function of the first kind of order one
 
     Parameters
