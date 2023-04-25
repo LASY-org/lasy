399fdc0f06a123d80b3436d731d9b61f34366de6
rob.shalloo <rob.shalloo@desy.de>
2023-04-26 01:02:17 +0200

    incorporate experimental data on laser temporal profile

--- lasy/profiles/longitudinal/__init__.py
+++ lasy/profiles/longitudinal/__init__.py
@@ -1,3 +1,5 @@
 from .gaussian_profile import GaussianLongitudinalProfile
+from .longitudinal_profile_from_data import LongitudinalProfileFromData
 
-__all__ = ["GaussianLongitudinalProfile"]
+__all__ = ["GaussianLongitudinalProfile",
+           "LongitudinalProfileFromData"]
