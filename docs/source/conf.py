a21584b09703badc46912db5d7b7f54f0225e98c
Remi Lehe <remi.lehe@normalesup.org>
2023-04-27 09:13:41 -0700

    Update version in documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = "%s, LASY-org" % date.today().year
 author = "LASY-org"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0"
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
