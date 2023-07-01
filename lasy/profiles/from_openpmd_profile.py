30ff9261bc43e80ca155a1ae8d8761f8db1767d0
Maxence Thevenet <maxence.thevenet@desy.de>
2023-07-01 18:25:50 +0200

    add Profile to read from openPMD file

---
+++ lasy/profiles/from_openpmd_profile.py
@@ -0,0 +1,109 @@
+import numpy as np
+from scipy.signal import hilbert
+from scipy.constants import c
+from openpmd_viewer import OpenPMDTimeSeries
+from .profile import Profile
+from .from_array_profile import FromArrayProfile
+
+
+class FromOpenPMDProfile(Profile):
+    r"""
+    Profile defined from an openPMD file.
+
+    Parameters
+    ----------
+    path : string
+        Path to the openPMD file containing the laser field or envelope.
+        Passed directly OpenPMDTimeSeries.
+
+    iteration : int
+        Iteration at which the argument is read.
+        Passed directly OpenPMDTimeSeries.
+
+    pol : list of 2 complex numbers (dimensionless)
+        Polarization vector. It corresponds to :math:`p_u` in the above
+        formula ; :math:`p_x` is the first element of the list and
+        :math:`p_y` is the second element of the list. Using complex
+        numbers enables elliptical polarizations.
+
+    field : string
+        Name of the field containing the laser pulse
+        Passed directly OpenPMDTimeSeries.
+
+    coord : string
+        Name of the field containing the laser pulse
+        Passed directly OpenPMDTimeSeries.
+
+    envelope : boolean
+        Whether the file represents a laser envelope.
+        If not, the envelope is obtained from the electric field
+        using a Hilbert transform
+
+    wavelength : float (in meter)
+        The main laser wavelength :math:`\\lambda_0` of the laser, which defines
+        :math:`\\omega_0`, according to :math:`\\omega_0 = 2\\pi c/\\lambda_0`.
+        if envelope is True, this argument is currently required.
+        if envelope is False, this argument is optional: if not specified, the
+        central wavelength is obtained from the Hilbert transform.
+    """
+
+    def __init__(self, path, iteration, pol, field, coord=None,
+                 envelope=False, wavelength=None):
+
+        ts = OpenPMDTimeSeries(path)
+        F, m = ts.get_field(iteration=iteration, field=field, coord=coord, theta=None)
+        assert m.axes in [
+            {0: 'x', 1: 'y', 2: 'z'},
+            {0: 'z', 1: 'y', 2: 'x'},
+            {0: 'x', 1: 'y', 2: 't'},
+            {0: 't', 1: 'y', 2: 'x'}
+        ]
+
+        if m.axes in [{0: 'z', 1: 'y', 2: 'x'}, {0: 't', 1: 'y', 2: 'x'}]:
+            F = F.swapaxes(0,2)
+
+        if 'z' in m.axes.values():
+            dt = m.dz / c
+            t = (m.z-m.z[0]) / c
+        else:
+            dt = m.dt
+            t = m.t
+        axes = {'x':m.x, 'y':m.y, 't':t}
+
+        # If array does not contain the envelope but the electric field,
+        # extract the envelope with a Hilbert transform
+        if envelope == False:
+
+            # Assumes z is last dimension!
+            h = hilbert(F)
+
+            if 'z' in m.axes.values():
+                # Flip to get complex envelope in t assuming z = -c*t
+                h = np.flip(h, axis=2)
+
+            # Get central wavelength from array
+            phase = np.unwrap(np.angle(h))
+            omg0_h = -np.average(np.diff(phase), weights=\
+                                 .5*(np.abs(h[:,:,1:])+np.abs(h[:,:,:-1])))/dt
+            wavelength_loc = 2*np.pi*c/omg0_h
+            if wavelength is not None:
+                wavelength_loc = wavelength
+            array = h*np.exp(1j*omg0_h*t)
+        else:
+            # in principle, if the openPMD file represents an envelope,
+            # the wavelength should be read from the file directly
+            assert wavelength is not None
+            wavelength_loc = wavelength
+            array = F
+
+        super().__init__(wavelength_loc, pol)
+
+        axes_order = ['x', 'y', 't']
+        self.profile = FromArrayProfile(wavelength=wavelength_loc, pol=pol,
+                                        array=array, axes=axes,
+                                        axes_order=axes_order)
+
+    def evaluate(self, x, y, t):
+        """Return the envelope field of the scaled profile."""
+        envelope = self.profile.evaluate(x, y, t)
+        return envelope
