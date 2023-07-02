c36c4b0c263ed461d84076659eb06d6c52318fbc
Maxence Thevenet <maxence.thevenet@desy.de>
2023-07-02 09:07:21 +0200

    fix

--- docs/source/api/profiles/index.rst
+++ docs/source/api/profiles/index.rst
@@ -13,5 +13,6 @@ Typically a laser will be constructed through a combination of two classes ``Lon
    gaussian
    combined_profile
    from_array_profile
+   from_openpmd_profile
    longitudinal/index
    transverse/index
--- lasy/profiles/__init__.py
+++ lasy/profiles/__init__.py
@@ -1,9 +1,11 @@
 from .combined_profile import CombinedLongitudinalTransverseProfile
 from .gaussian_profile import GaussianProfile
 from .from_array_profile import FromArrayProfile
+from .from_openpmd_profile import FromOpenPMDProfile
 
 __all__ = [
     "CombinedLongitudinalTransverseProfile",
     "GaussianProfile",
     "FromArrayProfile",
+    "FromOpenPMDProfile"
 ]
