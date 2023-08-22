629a3a1865c5c1a49458849413742523d66b90b7
Ángel Ferran Pousa <angel.ferran.pousa@desy.de>
2023-08-22 23:13:13 +0200

    Fix codeQL

--- tests/test_laser_utils.py
+++ tests/test_laser_utils.py
@@ -23,8 +23,8 @@ def get_gaussian_laser(dim):
     if dim == "rt":
         lo = (0e-6, -60e-15)
         hi = (25e-6, +60e-15)
-        npoints = (100, 100)
-    elif dim == "xyt":
+        npoints = (100, 100)    
+    else:  # dim == "xyt":
         lo = (-25e-6, -25e-6, -60e-15)
         hi = (+25e-6, +25e-6, +60e-15)
         npoints = (100, 100, 100)
