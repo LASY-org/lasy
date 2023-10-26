9e65b6757b7cae7058fbb0fb408a347660ec3690
Remi Lehe <remi.lehe@normalesup.org>
2023-10-26 10:20:13 -0700

    Make progress bar optional

--- lasy/laser.py
+++ lasy/laser.py
@@ -139,7 +139,7 @@ class Laser:
         else:
             raise ValueError(f'kind "{kind}" not recognized')
 
-    def propagate(self, distance, nr_boundary=None, backend="NP"):
+    def propagate(self, distance, nr_boundary=None, backend="NP", show_progress=True):
         """
         Propagate the laser pulse by the distance specified.
 
@@ -154,6 +154,8 @@ class Laser:
             Only used for ``'rt'``.
         backend : string (optional)
             Backend used by axiprop (see axiprop documentation).
+        show_progress : bool (optional)
+            Whether to show a progress bar when performing the computation
         """
         time_axis_indx = -1
 
@@ -211,7 +213,7 @@ class Laser:
             for i_m in range(self.grid.azimuthal_modes.size):
                 transform_data = np.transpose(field_fft[i_m]).copy()
                 self.prop[i_m].step(
-                    transform_data, distance, overwrite=True, show_progress=True
+                    transform_data, distance, overwrite=True, show_progress=show_progress
                 )
                 field_fft[i_m, :, :] = np.transpose(transform_data).copy()
         else:
@@ -229,7 +231,7 @@ class Laser:
                 )
             # Propagate the spectral image
             transform_data = np.transpose(field_fft).copy()
-            self.prop.step(transform_data, distance, overwrite=True, show_progress=True)
+            self.prop.step(transform_data, distance, overwrite=True, show_progress=show_progress)
             field_fft[:, :, :] = np.transpose(transform_data).copy()
 
         # Choose the time translation assuming propagation at v=c
