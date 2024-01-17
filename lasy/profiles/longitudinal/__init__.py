7538cd4176be8024702412bccc903b6e875dd15b
lucafedeli88 <luca.fedeli@cea.fr>
2024-01-17 10:56:58 +0100

    fixed bug

--- lasy/profiles/longitudinal/__init__.py
+++ lasy/profiles/longitudinal/__init__.py
@@ -3,7 +3,7 @@ from .gaussian_profile import GaussianLongitudinalProfile
 from .longitudinal_profile_from_data import LongitudinalProfileFromData
 
 __all__ = [
-    "CosLongitudinalProfile",
+    "CosineLongitudinalProfile",
     "GaussianLongitudinalProfile",
     "LongitudinalProfileFromData",
 ]
