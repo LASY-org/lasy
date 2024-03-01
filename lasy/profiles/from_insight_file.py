0d3d8f0ca6e42e53a2d89ddd438472a89d470354
Igor Andriyash <igor.andriyash@gmail.com>
2024-03-01 15:37:32 +0100

    few typo and style fixed added check-and-correct procedure for complex field convention

--- lasy/profiles/from_insight_file.py
+++ lasy/profiles/from_insight_file.py
@@ -1,13 +1,12 @@
 import numpy as np
 import h5py
 from scipy.constants import c
-
 from .from_array_profile import FromArrayProfile
 
 
 class FromInsightFile(FromArrayProfile):
     r"""
-    Profile defined from numpy array directly.
+    Profile defined from insight measurement.
 
     Parameters
     ----------
@@ -27,7 +26,7 @@ class FromInsightFile(FromArrayProfile):
             data = np.asanyarray(hf["data/Exyt_0"][()], dtype=np.complex128, order="C")
             t = np.asanyarray(hf["scales/t"][()], dtype=np.float64, order="C")
             x = np.asanyarray(hf["scales/x"][()], dtype=np.float64, order="C")
-            y = np.asanyarray(hf["scales/x"][()], dtype=np.float64, order="C")
+            y = np.asanyarray(hf["scales/y"][()], dtype=np.float64, order="C")
 
         # convert data and axes to SI units
         t *= 1e-15
@@ -35,21 +34,24 @@ class FromInsightFile(FromArrayProfile):
         y *= 1e-3
 
         # get central frequency from the field on axis,
-        env = data[data.shape[0] // 2, data.shape[1] // 2, :]
-        omega_array = np.abs(np.gradient(np.unwrap(np.angle(env)), t))
+        field_onaxis = data[data.shape[0] // 2, data.shape[1] // 2, :]
+        omega_array = -np.gradient(np.unwrap(np.angle(field_onaxis)), t)
         # using peak field frequency
-        omega0 = omega_array[np.abs(env).argmax()]
+        omega0 = omega_array[np.abs(field_onaxis).argmax()]
         # or "center of mass" frequency
         ## omega0 = np.average(omega_array, weights=np.abs(env)**2)
 
-        wavelength = 2 * np.pi * c / omega0
+        # check the complex field convention and correct if needed
+        if omega0<0:
+            omega0 *= -1
+            data = np.conj(data)
+            print('Warning: input field will be conjugated')
 
-        # conjugarte for proper frequency sign convention
-        data = np.conjugate(data)
         # remove the envelope frequencey
         data *= np.exp(1j * omega0 * t[None, None, :])
 
         # created LASY profile using FromArrayProfile class
+        wavelength = 2 * np.pi * c / omega0
         dim = "xyt"
         axes = {"x": x, "y": y, "t": t}
         super().__init__(
