1b63f38f8abf9b2687a5681bea03e3f085449b85
Remi Lehe <remi.lehe@normalesup.org>
2024-03-18 10:13:55 -0700

    Expose base class for longitudinal profile to the user

--- lasy/profiles/longitudinal/__init__.py
+++ lasy/profiles/longitudinal/__init__.py
@@ -1,9 +1,11 @@
 from .cosine_profile import CosineLongitudinalProfile
 from .gaussian_profile import GaussianLongitudinalProfile
 from .longitudinal_profile_from_data import LongitudinalProfileFromData
+from .longitudinal_profile import LongitudinalProfile
 
 __all__ = [
     "CosineLongitudinalProfile",
     "GaussianLongitudinalProfile",
     "LongitudinalProfileFromData",
+    "LongitudinalProfile"
 ]
