96254ba96982a6e283ee46cd749e126f5c32a465
RTSandberg <rsandberg@lbl.gov>
2024-04-18 17:48:16 -0700

    more automated refactoring

--- lasy/profiles/__init__.py
+++ lasy/profiles/__init__.py
@@ -2,12 +2,10 @@ from .combined_profile import CombinedLongitudinalTransverseProfile
 from .gaussian_profile import GaussianProfile
 from .from_array_profile import FromArrayProfile
 from .from_openpmd_profile import FromOpenPMDProfile
-from .speckled.speckle_profile import SpeckleProfile
 
 __all__ = [
     "CombinedLongitudinalTransverseProfile",
     "GaussianProfile",
     "FromArrayProfile",
     "FromOpenPMDProfile",
-    # "SpeckleProfile",
 ]
