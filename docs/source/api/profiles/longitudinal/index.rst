08b38774cdc88aed2dc43a3cbc29eb0ba0a77b50
Kale Weichman <46941919+kale-j@users.noreply.github.com>
2024-04-29 12:08:07 -0400

    Add super-Gaussian longitudinal profile (#232)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- docs/source/api/profiles/longitudinal/index.rst
+++ docs/source/api/profiles/longitudinal/index.rst
@@ -7,4 +7,5 @@ Longitudinal Profiles
 
    cosine
    gaussian
+   super_gaussian_profile
    longitudinal_profile_from_data
--- lasy/profiles/longitudinal/__init__.py
+++ lasy/profiles/longitudinal/__init__.py
@@ -1,9 +1,11 @@
 from .cosine_profile import CosineLongitudinalProfile
 from .gaussian_profile import GaussianLongitudinalProfile
+from .super_gaussian_profile import SuperGaussianLongitudinalProfile
 from .longitudinal_profile_from_data import LongitudinalProfileFromData
 
 __all__ = [
     "CosineLongitudinalProfile",
     "GaussianLongitudinalProfile",
+    "SuperGaussianLongitudinalProfile",
     "LongitudinalProfileFromData",
 ]
