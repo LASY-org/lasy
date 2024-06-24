b7c97a12df955ae39c02a6bdc76810f6cb4e78bf
Remi Lehe <remi.lehe@normalesup.org>
2024-06-24 12:38:48 -0700

    Add possibility to choose the number of points in theta

--- lasy/laser.py
+++ lasy/laser.py
@@ -44,6 +44,17 @@ class Laser:
         Only used if ``dim`` is ``'rt'``. The number of azimuthal modes
         used in order to represent the laser field.
 
+    n_theta_evals: int (optional)
+        Only used if ``dim`` is ``'rt'``. The number of points in the theta
+        (azimuthal) direction at which to evaluate the laser field, before
+        decomposing it into ``n_azimuthal_modes`` azimuthal modes. By default,
+        this is set to ``2*n_azimuthal_modes - 1``. However, for highly asymmetrical
+        profiles, it may be necessary to increase this number.
+
+        For instance, using ``n_theta_evals=20`` and ``n_azimuthal_modes=1``
+        will evaluate the laser field at 20 points in the azimuthal direction
+        and then average the values to extract the amplitude of the azimuthal mode 0.
+
     Examples
     --------
     >>> import matplotlib.pyplot as plt
@@ -92,7 +103,7 @@ class Laser:
     >>>         axes[step].set(ylabel='r (µm)')
     """
 
-    def __init__(self, dim, lo, hi, npoints, profile, n_azimuthal_modes=1):
+    def __init__(self, dim, lo, hi, npoints, profile, n_azimuthal_modes=1, n_theta_evals=None):
         self.grid = Grid(dim, lo, hi, npoints, n_azimuthal_modes)
         self.dim = dim
         self.profile = profile
@@ -102,17 +113,23 @@ class Laser:
             x, y, t = np.meshgrid(*self.grid.axes, indexing="ij")
             self.grid.field[...] = profile.evaluate(x, y, t)
         elif self.dim == "rt":
-            # Generate 2*n_azimuthal_modes - 1 evenly-spaced values of
-            # theta, to evaluate the laser
-            n_theta = 2 * self.grid.n_azimuthal_modes - 1
-            theta1d = 2 * np.pi / n_theta * np.arange(n_theta)
+            if n_theta_evals is None:
+                # Generate 2*n_azimuthal_modes - 1 evenly-spaced values of
+                # theta, to evaluate the laser
+                n_theta_evals = 2 * self.grid.n_azimuthal_modes - 1
+            # Make sure that there are enough points to resolve the azimuthal modes
+            assert n_theta_evals >= 2 * self.grid.n_azimuthal_modes - 1
+            theta1d = 2 * np.pi / n_theta * np.arange(n_theta_evals)
             theta, r, t = np.meshgrid(theta1d, *self.grid.axes, indexing="ij")
             x = r * np.cos(theta)
             y = r * np.sin(theta)
             # Evaluate the profile on the generated grid
             envelope = profile.evaluate(x, y, t)
             # Perform the azimuthal decomposition
-            self.grid.field[...] = np.fft.ifft(envelope, axis=0)
+            azimuthal_modes = np.fft.ifft(envelope, axis=0)
+            self.grid.field[:n_azimuthal_modes] = azimuthal_modes[:n_azimuthal_modes]
+            if n_azimuthal_modes > 1:
+                self.grid.field[-n_azimuthal_modes+1:] = azimuthal_modes[-n_azimuthal_modes+1:]
 
         # For profiles that define the energy, normalize the amplitude
         if hasattr(profile, "laser_energy"):
