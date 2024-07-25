fb72ba6496af6be932513de33a67b1cd1d032df4
Remi Lehe <remi.lehe@normalesup.org>
2024-07-25 04:48:52 -0700

    Slight modifications

--- tests/test_polynomial_spectral_phase.py
+++ tests/test_polynomial_spectral_phase.py
@@ -47,10 +47,11 @@ def test_gdd():
     E_before = laser.grid.get_temporal_field()
     laser.apply_optics(dazzler)
     E_after = laser.grid.get_temporal_field()
+    # Extract peak field before dazzler
+    E0 = abs(E_before[50,50]).max()
 
     # Compute the analtical expression in real space for a Gaussian
     t = np.linspace(laser.grid.lo[-1], laser.grid.hi[-1], laser.grid.npoints[-1])
-    E0 = E_before.max()
     stretch_factor = 1 - 2j * gdd / tau**2
     E_analytical = (
         E0 * np.exp(-1.0 / stretch_factor * (t / tau) ** 2) / stretch_factor**0.5
@@ -83,13 +84,14 @@ def test_tod():
     E_before = laser.grid.get_temporal_field()
     laser.apply_optics(dazzler)
     E_after = laser.grid.get_temporal_field()
-
-    # Only compare data in the post-pulse region (t>t_peak+tau),
-    # where the stationary phase approximation is valid
-    E_compare = abs( E_after[50, 50, t>t_peak + tau] )
-    t = t[t>t_peak + tau]
-
+    # Extract peak field before dazzler
     E0 = abs(E_before[50,50]).max()
+
+    # Compare data in the post-pulse region to the stationary phase approximation.
+    # The stationary phase approximation result was obtained under the additional
+    # assumption t >> tau^4/tod, so we only compare the data in this region
+    E_compare = abs( E_after[50, 50, t>t_peak + 2*tau**4/tod] ) # On-axis field
+    t = t[t>t_peak + 2*tau**4/tod]
     prediction = abs(
             2* E0 * tau/(8*tod*t)**.25
             * np.exp( - tau**2*t/(2*tod) )
@@ -98,5 +100,5 @@ def test_tod():
         )
 
     # Compare the on-axis field with the analytical formula
-    tol = 3e-2
+    tol = 2.4e-2
     assert np.all( abs(E_compare - prediction)/abs(E0) < tol )
