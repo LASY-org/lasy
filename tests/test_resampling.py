a31e817b7cb8d18544d82d4cbc088b755108dd0e
NadezhdaKHACHAT <148543195+NadezhdaKHACHAT@users.noreply.github.com>
2024-07-30 12:48:11 +0200

    Update test_resampling.py

--- tests/test_resampling.py
+++ tests/test_resampling.py
@@ -1,8 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+This test checks the implementation of the resampling propagator
+by initializing a Gaussian pulse in the near field, and
+propagating it through a parabolic mirror, and then to
+the focal position (radial axis is resampled to accomodate the new size of the beam) ; we then check that the waist as the
+expected value in the far field (i.e. in the focal plane)
+"""
+
 import numpy as np
 
-from lasy.laser import Grid, Laser
+from lasy.laser import Laser, Grid
 from lasy.optical_elements import ParabolicMirror
 from lasy.profiles.gaussian_profile import GaussianProfile
+
 
 wavelength = 0.8e-6
 w0 = 5.0e-3  # m, initialized in near field
@@ -17,14 +27,16 @@ tau = 30.0e-15  # s
 dim = "rt"
 lo = (0e-3, -90e-15)
 hi = (15e-3, +90e-15)
-npoints = (250, 30)
+npoints = (500, 100)
 
 # Initialize the laser
 gaussian_profile = GaussianProfile(wavelength, pol, laser_energy, w0, tau, t_peak)
 laser = Laser(dim, lo, hi, npoints, gaussian_profile)
 
 def get_w0(laser):
-    A2 = (np.abs(laser.grid.field[0, :, :]) ** 2).sum(-1)
+    # Calculate the laser waist
+    field = laser.grid.get_temporal_field()
+    A2 = (np.abs(field[0, :, :]) ** 2).sum(-1)
     ax = laser.grid.axes[0]
     if ax[0] > 0:
         A2 = np.r_[A2[::-1], A2]
@@ -38,43 +50,22 @@ def get_w0(laser):
     return sigma
 
 
+
 def check_resampling(laser, new_grid):
-    # Focus down the laser and propagate using resampling
+    # Focus down the laser and propagate 
     f0 = 2.0  # focal distance in m
-<<<<<<< HEAD
-    laser.apply_optics(ParabolicMirror(f=f0))
-    laser.propagate((f0), grid=new_grid, show_progress=False) # resample the radial grid
-
-=======
-    laser.apply_optics(ParabolicMirror(f=f0))
-    laser.propagate((f0), grid=new_grid)  # resample the radial grid
-
->>>>>>> 3d48d959113146337659ef0472e3ecd82f591793
+    laser.apply_optics(ParabolicMirror(f=f0))    
+    laser.propagate((f0), nr_boundary=128, grid=new_grid) # resample the radial grid
+    
     # Check that the value is the expected one in the near field
     w0_num = get_w0(laser)
     w0_theor = wavelength * f0 / (np.pi * w0)
     err = 2 * np.abs(w0_theor - w0_num) / (w0_theor + w0_num)
     assert err < 1e-3
-
-
+    
 def test_resampling():
     # Define the new grid for the laser
-<<<<<<< HEAD
-    new_r_max = 300.e-6
+    new_r_max = 300e-6
     new_grid = Grid(dim, lo, (new_r_max, hi[1]), npoints, n_azimuthal_modes=laser.grid.n_azimuthal_modes)
-
+    
     check_resampling(laser, new_grid)
-
-=======
-    new_r_max = 300.0e-6
-    new_grid = Grid(
-        dim,
-        lo,
-        (new_r_max, hi[1]),
-        npoints,
-        n_azimuthal_modes=laser.grid.n_azimuthal_modes,
-    )
-
-    laser = Laser(dim, lo, hi, npoints, gaussian_profile)
-    check_resampling(laser, new_grid)
->>>>>>> 3d48d959113146337659ef0472e3ecd82f591793
