da6d2ab7319a4c4891596d61e6cd7759eb8537cf
cdmccombs <69375785+cdmccombs@users.noreply.github.com>
2024-08-05 12:04:23 -0700

    Update __init__.py

--- lasy/optical_elements/__init__.py
+++ lasy/optical_elements/__init__.py
@@ -3,4 +3,4 @@ from .parabolic_mirror import ParabolicMirror
 from .polynomial_spectral_phase import PolynomialSpectralPhase
 from .axicon import Axicon
 
-__all__ = ["ParabolicMirror", "PolynomialSpectralPhase", "Axicon"]
+__all__ = ["ParabolicMirror", "PolynomialSpectralPhase", "Axiparabola"]
