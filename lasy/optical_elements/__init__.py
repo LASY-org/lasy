17ade8df0c270b20f3d40c1a35ea7f441f2568d7
Remi Lehe <remi.lehe@normalesup.org>
2024-08-05 14:30:33 -0700

    Apply suggestions from code review

--- lasy/optical_elements/__init__.py
+++ lasy/optical_elements/__init__.py
@@ -3,4 +3,4 @@ from .axiparabola import Axiparabola
 from .parabolic_mirror import ParabolicMirror
 from .polynomial_spectral_phase import PolynomialSpectralPhase
 
-__all__ = ["ParabolicMirror", "PolynomialSpectralPhase", "Axicon"]
+__all__ = ["ParabolicMirror", "PolynomialSpectralPhase", "Axiparabola", "Axicon"]
