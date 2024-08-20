de3cd11feea13f3526064dcf141cb1edb0cb1fa7
NadezhdaKHACHAT <148543195+NadezhdaKHACHAT@users.noreply.github.com>
2024-08-20 16:02:41 +0200

    Update test_resampling.py (changing number of radial grid points while resampling)

--- tests/test_resampling.py
+++ tests/test_resampling.py
@@ -9,9 +9,10 @@ expected value in the far field (i.e. in the focal plane)
 
 import numpy as np
 
-from lasy.laser import Grid, Laser
+from lasy.laser import Laser, Grid
 from lasy.optical_elements import ParabolicMirror
 from lasy.profiles.gaussian_profile import GaussianProfile
+
 
 wavelength = 0.8e-6
 w0 = 5.0e-3  # m, initialized in near field
@@ -32,7 +33,6 @@ npoints = (500, 100)
 gaussian_profile = GaussianProfile(wavelength, pol, laser_energy, w0, tau, t_peak)
 laser = Laser(dim, lo, hi, npoints, gaussian_profile)
 
-
 def get_w0(laser):
     # Calculate the laser waist
     field = laser.grid.get_temporal_field()
@@ -49,29 +49,22 @@ def get_w0(laser):
 
     return sigma
 
-
 def check_resampling(laser, new_grid):
-    # Focus down the laser and propagate
+    # Focus down the laser and propagate 
     f0 = 2.0  # focal distance in m
-    laser.apply_optics(ParabolicMirror(f=f0))
-    laser.propagate((f0), nr_boundary=128, grid=new_grid)  # resample the radial grid
-
+    laser.apply_optics(ParabolicMirror(f=f0))    
+    laser.propagate((f0), nr_boundary=128, grid=new_grid) # resample the radial grid
+    
     # Check that the value is the expected one in the near field
     w0_num = get_w0(laser)
     w0_theor = wavelength * f0 / (np.pi * w0)
     err = 2 * np.abs(w0_theor - w0_num) / (w0_theor + w0_num)
     assert err < 1e-3
-
-
+    
 def test_resampling():
     # Define the new grid for the laser
     new_r_max = 300e-6
-    new_grid = Grid(
-        dim,
-        lo,
-        (new_r_max, hi[1]),
-        npoints,
-        n_azimuthal_modes=laser.grid.n_azimuthal_modes,
-    )
-
+    npoints_new = (50, 100)
+    new_grid = Grid(dim, lo, (new_r_max, hi[1]), npoints_new,
+                     n_azimuthal_modes=laser.grid.n_azimuthal_modes)
     check_resampling(laser, new_grid)
