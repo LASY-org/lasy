1d16dd16deb3c70c659c46a0f85fdf857aee493a
Xingjian Hui <151739545+huixingjian@users.noreply.github.com>
2024-10-18 23:12:16 +0200

    Update gaussian_profile.py

--- lasy/profiles/longitudinal/gaussian_profile.py
+++ lasy/profiles/longitudinal/gaussian_profile.py
@@ -114,8 +114,9 @@ class GaussianLongitudinalProfile(LongitudinalProfile):
             specified points. This array has the same shape as the array t.
         """
         inv_tau2 = self.tau ** (-2)
-        inv_complex_waist_2 = 1.0 / (
-            self.w0**2 * (1.0 + 2.0j * self.z_foc_over_zr / (self.k0 * self.w0**2))
+        inv_complex_waist_2 = (
+            1.0 / (self.w0**2 * (1.0 + 2.0j * self.z_foc_over_zr / (self.k0 * self.w0**2)))
+            if self.w0 else 0
         )
         stretch_factor = (
             1
