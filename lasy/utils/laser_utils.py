70260c395b292ff1eff71a311e56769da45d78cb
huixingjian <xingjian.hui@desy.de>
2024-10-30 00:46:09 +0100

    add np.max

--- lasy/utils/laser_utils.py
+++ lasy/utils/laser_utils.py
@@ -1018,9 +1018,9 @@ def get_STC(dim, grid, k0):
         for i in range(Phi2_mean.shape[0]):
             for j in range(Phi2_mean.shape[1]):
                 # Calculate roots for each element
-                phi2[i, j] = np.roots(
+                phi2[i, j] =  np.max(np.roots(
                     [4 * Phi2_mean[i, j], -4, tau**4 * Phi2_mean[i, j]]
-                )
+                ))
         phi2nu = np.average(phi2 * derivative_x, weights=weight)
 
         print(phi2nu)
