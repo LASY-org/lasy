fix beta calculation

lasy/utils/laser_utils.py
@@ -967,6 +967,7 @@ def get_STC(dim, grid, k0):
         "zeta": 0,
         "stc_theta_zeta": 0,
         "beta": 0,
+        "stc_theta_beta": 0,
         "pft": 0,
         "stc_theta_pft": 0,
     }
@@ -974,7 +975,8 @@ def get_STC(dim, grid, k0):
     # Get temporal and spectral field
     env = grid.get_temporal_field()
     env_abs = np.abs(env**2)
-    env_spec = np.abs(grid.get_spectral_field())
+    env_spec = grid.get_spectral_field()
+    env_spec_abs = np.abs(env_spec**2)
 
     # Get the spectral axis
     dt = grid.dx[-1]
@@ -998,16 +1000,16 @@ def get_STC(dim, grid, k0):
         return STC_fac
     if dim == "xyt":
         # Calculate dx and dy in (x,y,omega) space
-        weight_x_3d = np.transpose(env_spec, (2, 1, 0))
-        weight_y_3d = np.transpose(env_spec, (2, 0, 1))
+        weight_x_3d = np.transpose(env_spec_abs, (2, 1, 0))
+        weight_y_3d = np.transpose(env_spec_abs, (2, 0, 1))
         xda = np.sum(grid.axes[0] * weight_x_3d, axis=2) / np.sum(weight_x_3d, axis=2)
         yda = np.sum(grid.axes[1] * weight_y_3d, axis=2) / np.sum(weight_y_3d, axis=2)
 
         # Calculate zeta_x and zeta_y
         derivative_x_zeta = np.gradient(xda, omega, axis=0)
         derivative_y_zeta = np.gradient(yda, omega, axis=0)
-        weight_x_2d = np.mean(env_spec, axis=0)
-        weight_y_2d = np.mean(env_spec, axis=1)
+        weight_x_2d = np.mean(env_spec_abs, axis=0)
+        weight_y_2d = np.mean(env_spec_abs, axis=1)
         zeta_x = np.average(derivative_x_zeta.T, weights=weight_x_2d)
         zeta_y = np.average(derivative_y_zeta.T, weights=weight_y_2d)
         STC_fac["stc_theta_zeta"] = np.arctan2(zeta_y, zeta_x)
@@ -1017,16 +1019,17 @@ def get_STC(dim, grid, k0):
         )
 
         # Calculate propagating angle
-        pphi_py = np.gradient(env_spec, grid.dx[1], axis=1)
-        pphi_px = np.gradient(env_spec, grid.dx[0], axis=0)
+        phi_envelop_abs = np.unwrap(np.array(np.arctan2(env_spec.imag, env_spec.real)), axis=2)
+        pphi_py = np.gradient(phi_envelop_abs, grid.dx[1], axis=1)
+        pphi_px = np.gradient(phi_envelop_abs, grid.dx[0], axis=0)
 
         angle_y = (
-            np.sum(pphi_py * env_spec, axis=(0, 1)) / np.sum(env_spec, axis=(0, 1))
+            np.sum(pphi_py * env_spec_abs, axis=(0, 1)) / np.sum(env_spec_abs, axis=(0, 1))
         ) / k0
         angle_x = (
-            np.sum(pphi_px * env_spec, axis=(0, 1)) / np.sum(env_spec, axis=(0, 1))
+            np.sum(pphi_px * env_spec_abs, axis=(0, 1)) / np.sum(env_spec_abs, axis=(0, 1))
         ) / k0
-        weight_omega_1d = np.mean(env_spec, axis=(0, 1))
+        weight_omega_1d = np.mean(env_spec_abs, axis=(0, 1))
         derivative_x_beta = np.gradient(angle_y, omega)
         derivative_y_beta = np.gradient(angle_x, omega)
         beta_x=np.average(derivative_x_beta,weights=weight_omega_1d)