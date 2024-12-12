f0ca9f9dfea962b85d684b1f48bd647bf4e42572
Xingjian Hui <151739545+huixingjian@users.noreply.github.com>
2024-12-12 16:19:35 +0100

    Update gaussian_profile.py

--- lasy/profiles/gaussian_profile.py
+++ lasy/profiles/gaussian_profile.py
@@ -189,7 +189,8 @@ class STCGaussianProfile(Profile):
         zeta=0,
         stc_theta=0,
     ):
-        super().__init__(wavelength)
+        super().__init__(wavelength,
+            pol)
         self.tau = tau
         self.t_peak = t_peak
         self.cep_phase = cep_phase
