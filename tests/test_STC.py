204568a8202590d30b5e6af7e30863a94202407a
Xingjian Hui <151739545+huixingjian@users.noreply.github.com>
2024-12-17 15:35:59 +0100

    Update test_STC.py

--- tests/test_STC.py
+++ tests/test_STC.py
@@ -3,7 +3,7 @@ import scipy.constants as scc
 
 from lasy.laser import Laser
 from lasy.profiles.gaussian_profile import GaussianProfile
-
+from lasy.utils.laser_utils import get_phi2, get_zeta, get_beta
 # Create profile.
 profile = GaussianProfile(
     wavelength=0.6e-6,  # m
