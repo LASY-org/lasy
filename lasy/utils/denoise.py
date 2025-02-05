61e7db3bd7b811ec7b3d2f7b6b830c4fc7337e2d
Parth <96476618+Paaaaarth@users.noreply.github.com>
2025-02-05 13:32:46 +0100

    minor changes

--- lasy/utils/denoise.py
+++ lasy/utils/denoise.py
@@ -72,4 +72,4 @@ def hg_reconstruction(
     energy_loss = 1 - energy_new
     print(f"Energy loss: {energy_loss * 100:.2f}%")
 
-    return transverse_profile_cleaned, waist
+    return transverse_profile_cleaned, waistX, waistY
