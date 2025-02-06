55e435e5245626eddae0ac1dfc3e883ec068e610
delaossa <alberto.martinez.de.la.ossa@desy.de>
2025-02-06 09:49:38 +0100

    Polish code.

--- lasy/profiles/transverse/flattened_gaussian_profile.py
+++ lasy/profiles/transverse/flattened_gaussian_profile.py
@@ -1,8 +1,5 @@
-from math import factorial
-
 import numpy as np
-from scipy.special import genlaguerre, binom
-
+from scipy.special import binom
 from .transverse_profile import TransverseProfile
 
 
@@ -17,38 +14,48 @@ class FlattenedGaussianTransverseProfile(TransverseProfile):
     flatness of the transverse profile **far from focus**,
     and increases the number of rings **in the focal plane**.
 
+    The implementation of this class is directly copied from that in `FBPIC`
+    <https://github.com/fbpic/fbpic/blob/dev/fbpic/lpa_utils/laser/transverse_laser_profiles.py>.
+
     **In the focal plane** (:math:`z=z_f`), the profile translates to a
     laser with a transverse electric field:
 
     .. math::
 
         E(x,y,z=zf) \propto
-        \exp\left(-\frac{r^2}{(N+1)w_0^2}\right)
-        \sum_{n=0}^N c'_n L^0_n\left(\frac{2\,r^2}{(N+1)w_0^2}\right)
+        \exp\left(-\frac{r^2}{(N+1)w0^2}\right)
+        \sum_{n=0}^N c'_n L^0_n\left(\frac{2\,r^2}{(N+1)w0^2}\right)
 
         \mathrm{with} Laguerre polynomials :math:`L^0_n` and
         \qquad c'_n = \sum_{m=n}^{N}\frac{1}{2^m}\binom{m}{n}
 
-    - For :math:`N=0`, this is a Gaussian profile: :math:`E\propto\exp\left(-\frac{r^2}{w_0^2}\right)`.
+    - For :math:`N=0`, this is a Gaussian profile: :math:`E\propto\exp\left(-\frac{r^2}{w0^2}\right)`.
 
-    - For :math:`N\rightarrow\infty`, this is a Jinc profile: :math:`E\propto \frac{J_1(r/w_0)}{r/w_0}`.
+    - For :math:`N\rightarrow\infty`, this is a Jinc profile: :math:`E\propto \frac{J_1(r/w0)}{r/w0}`.
+
+    The equivalent expression **far from focus** is
+
+    .. math::
+
+        E(x,y,z=\infty) \propto
+        \exp\left(-\frac{(N+1)r^2}{w(z)^2}\right)
+        \sum_{n=0}^N \frac{1}{n!}\left(\frac{(N+1)\,r^2}{w(z)^2}\right)^n
+
+        \mathrm{with} \qquad w(z) = \frac{\lambda_0}{\pi w0}|z-z_{foc}|
+
+    - For :math:`N=0`, this is a Gaussian profile: :math:`E\propto\exp\left(-\frac{r^2}{w_(z)^2}\right)`.
+
+    - For :math:`N\rightarrow\infty`, this is a flat profile: :math:`E\propto \Theta(w(z)-r)`.
 
     Parameters
     ----------
-    w_0 : float (in meter)
+    w0 : float (in meter)
         The waist of the laser pulse,
         i.e. :math:`w_{0}` in the above formula.
     N: int
         Determines the "flatness" of the transverse profile, far from
         focus (see the above formula).
         Default: ``N=6`` ; somewhat close to an 8th order supergaussian.
-
-    p : int (dimensionless)
-        The order of Laguerre polynomial in the x direction
-        i.e. :math:`m` in the above formula.
-    m : int (dimensionless)
-        The order of Laguerre polynomial in the y direction
-        i.e. :math:`n` in the above formula.
     wavelength : float (in meter)
         The main laser wavelength :math:`\lambda_0` of the laser.
     z_foc : float (in meter), optional
@@ -68,26 +75,21 @@ class FlattenedGaussianTransverseProfile(TransverseProfile):
     not make this approximation.
     """
 
-    def __init__(self, w_0, N, wavelength, z_foc=0):
+    def __init__(self, w0, N, wavelength, z_foc=0):
         super().__init__()
-        self.w_0 = w_0
         # Ensure that N is an integer
         self.N = int(round(N))
-        self.wavelength = wavelength
-        self.z_foc = z_foc        
-        self.z_eval = -z_foc  # this links our observation position to Siegman's definition
-        self.k0 = 2 * np.pi / wavelength
-        # Calculate Rayleigh Length
-        zr = np.pi * w_0**2 / wavelength
-        self.inv_zr = 1. / zr
         # Calculate effective waist of the Laguerre-Gauss modes, at focus
-        self.w_foc = w_0*(self.N + 1)**.5
-
+        self.w_foc = w0 * (self.N + 1)**.5
+        # Calculate Rayleigh Length
+        self.zr = np.pi * self.w_foc**2 / wavelength
+        # Evaluation distance w.r.t focal position
+        self.z_eval = z_foc
         # Calculate the coefficients for the Laguerre-Gaussian modes
-        self.cn = np.empty(self.N+1)
-        for n in range(self.N+1):
-            m_values = np.arange(n, self.N+1)
-            self.cn[n] = np.sum((1./2)**m_values * binom(m_values,n)) / (self.N+1)
+        self.cn = np.empty(self.N + 1)
+        for n in range(self.N + 1):
+            m_values = np.arange(n, self.N + 1)
+            self.cn[n] = np.sum((1. / 2)**m_values * binom(m_values, n)) / (self.N + 1)
 
 
     def _evaluate(self, x, y):
@@ -106,34 +108,34 @@ class FlattenedGaussianTransverseProfile(TransverseProfile):
             Contains the value of the envelope at the specified points
             This array has the same shape as the arrays x, y
         """
-        z_eval = self.z_eval
-        diffract_factor = 1. + 1j * self.z_eval * self.inv_zr
-        w = self.w_foc * np.abs( diffract_factor )
-        psi = np.angle( diffract_factor )
+        # Term for wavefront curvature + Gouy phase
+        diffract_factor = 1. - 1j * self.z_eval / self.zr
+        w = self.w_foc * np.abs(diffract_factor)
+        psi = np.angle(diffract_factor)
         # Argument for the Laguerre polynomials
-        scaled_radius_squared = 2*( x**2 + y**2 ) / w**2
+        scaled_radius_squared = 2 * (x**2 + y**2) / w**2
 
         # Sum recursively over the Laguerre polynomials
-        laguerre_sum = np.zeros_like( x, dtype=np.complex128 )
-        for n in range(0, self.N+1):
+        laguerre_sum = np.zeros_like(x, dtype=np.complex128)
+        for n in range(0, self.N + 1):
             # Recursive calculation of the Laguerre polynomial
             # - `L` represents $L_n$
             # - `L1` represents $L_{n-1}$
             # - `L2` represents $L_{n-2}$
-            if n==0:
+            if n == 0:
                 L = 1.
-            elif n==1:
+            elif n == 1:
                 L1 = L
                 L = 1. - scaled_radius_squared
             else:
                 L2 = L1
                 L1 = L
-                L = (((2*n -1) - scaled_radius_squared) * L1 - (n - 1) * L2) / n
+                L = (((2 * n - 1) - scaled_radius_squared) * L1 - (n - 1) * L2) / n
             # Add to the sum, including the term for the additional Gouy phase
-            laguerre_sum += self.cn[n] * np.exp( - (2j* n) * psi ) * L
+            laguerre_sum += self.cn[n] * np.exp(-(2j * n) * psi) * L
 
-        # Final profile: multiply by n-independent propagation factors
-        exp_argument = - (x**2 + y**2) / (self.w_foc**2 * diffract_factor)
-        envelope = laguerre_sum * np.exp( exp_argument ) / diffract_factor
+        # Final envelope: multiply by n-independent propagation factors
+        exp_argument = -(x**2 + y**2) / (self.w_foc**2 * diffract_factor)
+        envelope = laguerre_sum * np.exp(exp_argument) / diffract_factor
 
         return envelope
