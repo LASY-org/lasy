389c7c2d40c04450acb2fc174e92ec46760d5cac
Anna Liisa Puchert <anna.puchert@desy.de>
2025-02-17 10:23:33 +0100

    Apply suggestions from code review
    Co-authored-by: Rob Shalloo <rob.shalloo@gmail.com>

--- lasy/optical_elements/intensity_mask.py
+++ lasy/optical_elements/intensity_mask.py
@@ -3,26 +3,31 @@ from lasy.optical_elements.optical_element import OpticalElement
 
 class IntensityMask(OpticalElement):
     """
-    Class for adding an radially symmetric intensity mask that acts as an aperture or a hole.
+    Class for a radially symmetric intensity mask.
+    
+    This creates an optical element which acts to mask out the intensity of a laser pulse. 
+    The mask is radially symmetric and can either mask intensity beyond a user defined radius, thus acting as
+    an aperture. Alternatively, the optica can mask intensity within a user defined radius, acting in this case as 
+    an optic with a hole.
 
     Parameters
     ----------
     R : float (in meter)
-        The Radius of the mask
+        The radius of the mask
     center: tuple (floats)
         Center of the mask. Default is (0,0)
-    aperture_type: string
+    mask_type: string
         Should be 'aperture' (default, allows light inside) or 'hole' (allows light outside).
 
     """
 
-    def __init__(self, R, center=(0, 0), aperture_type="aperture"):
-        assert aperture_type in ["aperture", "hole"], (
-            "aperture_type must be 'aperture' or 'hole'"
+    def __init__(self, R, center=(0, 0), mask_type="aperture"):
+        assert mask_type in ["aperture", "hole"], (
+            "mask_type must be 'aperture' or 'hole'"
         )
         self.R = R
         self.center = center
-        self.aperture_type = aperture_type
+        self.mask_type = mask_type
 
     def amplitude_multiplier(self, x, y, omega):
         """
@@ -46,7 +51,7 @@ class IntensityMask(OpticalElement):
         r_squared = (x - self.center[0]) ** 2 + (y - self.center[1]) ** 2
         mask = r_squared <= self.R**2  # True inside, False outside
 
-        if self.aperture_type == "aperture":
+        if self.mask_type == "aperture":
             return mask.astype(float)  # 1 inside, 0 outside
         else:  # "hole"
             return (~mask).astype(float)  # 0 inside, 1 outside
