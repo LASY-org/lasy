6051a0480b99740c406a68a7c2d889ae7f9e997a
rob.shalloo <rob.shalloo@desy.de>
2025-02-24 21:04:17 +0100

    fix import

--- lasy/profiles/transverse/__init__.py
+++ lasy/profiles/transverse/__init__.py
@@ -2,6 +2,7 @@ from .gaussian_profile import GaussianTransverseProfile
 from .hermite_gaussian_profile import HermiteGaussianTransverseProfile
 from .jinc_profile import JincTransverseProfile
 from .laguerre_gaussian_profile import LaguerreGaussianTransverseProfile
+from .flattened_gaussian_profile import FlattenedGaussianTransverseProfile
 from .super_gaussian_profile import SuperGaussianTransverseProfile
 from .transverse_profile import (
     ScaledTransverseProfile,
@@ -20,4 +21,5 @@ __all__ = [
     "TransverseProfile",
     "SummedTransverseProfile",
     "ScaledTransverseProfile",
+    "FlattenedGaussianTransverseProfile",
 ]
