0ed0d8026c38c14aa5f09722a6883f9b3f434baa
Parth Ramakant Patil <patilpar@mlsim39248.desy.de>
2025-03-03 13:34:56 +0100

    Trying to read FBPIC filr

--- lasy/profiles/from_openpmd_profile.py
+++ lasy/profiles/from_openpmd_profile.py
@@ -2,7 +2,7 @@ import numpy as np
 import openpmd_api as io
 from scipy.constants import c
 
-from lasy.utils.laser_utils import create_grid, vector_potential_to_field
+from lasy.utils.laser_utils import create_grid, vector_potential_to_field, field_to_envelope
 
 from .from_array_profile import FromArrayProfile
 
@@ -21,6 +21,19 @@ class FromOpenPMDProfile(FromArrayProfile):
 
     field : string
         Name of the field containing the laser pulse
+    
+    is_envelope : boolean
+        Whether the field to read represents a laser envelope.
+        If not, the envelope is obtained from the electric field
+        using a Hilbert transform. If not specified, lasy will try to guess
+        whether the field is an envelope by checking whether it is a complex
+        array.
+
+    phase_unwrap_nd : boolean (optional)
+        If True, the phase unwrapping is n-dimensional (2- or 3-D depending on dim).
+        If False, the phase unwrapping is done in t, treating each transverse cell
+        separately. This should be less accurate but faster.
+        If set to True, scikit-image must be installed.
     """
 
     def __init__(
@@ -28,19 +41,23 @@ class FromOpenPMDProfile(FromArrayProfile):
         path,
         iteration,
         field,
+        is_envelope=False,
+        phase_unwrap_nd=False,
+        polarization= None,
     ):
         series = io.Series(path, io.Access.read_only)
         i = series.iterations[iteration]
         m = i.meshes[field]
+        print(m.axis_labels)
+        print(m.shape)
         arr = m[io.Mesh_Record_Component.SCALAR].load_chunk()
-        print(arr.shape)
         series.flush()
-        # data = np.transpose(arr).copy()
-        # print(data.shape)
         omg0 = m.get_attribute("angularFrequency")
         wavelength = 2 * np.pi * c / omg0
-        pol = m.get_attribute("polarization")
-        grid = m.grid_global_offset
+        if polarization is None:
+            pol = m.get_attribute("polarization")
+        else:
+            pol = polarization
 
         if len(m.axis_labels) == 2:  # 'rt'
             n_r = int(arr.shape[2])
@@ -71,13 +88,21 @@ class FromOpenPMDProfile(FromArrayProfile):
                 "Error: The dimension of the field is not supported. The valid dimensions are 'rt' and 'xyt'."
             )
             return None
+        
+        # If array does not contain the envelope but the electric field,
+        # extract the envelope with a Hilbert transform
+        if is_envelope == True:
+            grid = create_grid(arr, axes, dim, is_envelope=is_envelope)
+            grid, omg0 = field_to_envelope(grid, dim, phase_unwrap_nd)
+            data = grid.get_temporal_field()[0]
+        else:
+            pass
 
         # If the filed is stored in form of a vector potential
         if m.get_attribute("envelopeField") == "normalized_vector_potential":
             if dim == "rt":
                 grid = create_grid(np.transpose(arr, (0, 2, 1)), axes, dim)
                 data_rt = vector_potential_to_field(grid, omg0)
-                print(data_rt.shape)
                 data = data_rt[0, :, :]
 
             else:
