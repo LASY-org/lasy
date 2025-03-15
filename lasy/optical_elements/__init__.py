749ba1f8b536758a86d4fc9ad67a95f2b8774b67
eichnert <timo.eichner@desy.de>
2025-03-15 21:11:18 +0100

    Added optical elements for a custom spectral phase and a spectral filter/spectrally dependent transmission

--- lasy/optical_elements/__init__.py
+++ lasy/optical_elements/__init__.py
@@ -3,6 +3,8 @@ from .axiparabola import Axiparabola
 from .parabolic_mirror import ParabolicMirror
 from .polynomial_spectral_phase import PolynomialSpectralPhase
 from .zernike_aberrations import ZernikeAberrations
+from .spectral_filter import SpectralFilter
+from .spectral_phase import SpectralPhase
 
 __all__ = [
     "ParabolicMirror",
@@ -10,4 +12,6 @@ __all__ = [
     "Axiparabola",
     "Axicon",
     "ZernikeAberrations",
+    "SpectralFilter",
+    "SpectralPhase",
 ]
