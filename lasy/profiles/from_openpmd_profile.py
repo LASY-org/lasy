0e0d67d01eb7bf00e0231c06effaeddff732e261
delaossa <alberto.martinez.de.la.ossa@desy.de>
2025-03-27 15:25:51 +0100

    Eliminate the need of `is_envelope` as argument.

--- lasy/profiles/from_openpmd_profile.py
+++ lasy/profiles/from_openpmd_profile.py
@@ -28,14 +28,6 @@ class FromOpenPMDProfile(FromArrayProfile):
 
     coordinate : string
         Name of the component of the field to be read.
-
-    omega0 : float
-        Angular frequency at which laser envelope is defined.
-
-    is_envelope : bool
-        Whether the field provided uses the (complex) envelope representation, as
-        used internally in lasy. If False, field is assumed to represent the
-        the full (real) electric field (with fast oscillations).
     """
 
     def __init__(
@@ -44,8 +36,6 @@ class FromOpenPMDProfile(FromArrayProfile):
         iteration,
         field,
         coordinate=None,
-        omega0=None,
-        is_envelope=True,
     ):
         # Read the data
         series = io.Series(path, io.Access.read_only)
@@ -57,12 +47,6 @@ class FromOpenPMDProfile(FromArrayProfile):
         else:
             array = m[coordinate].load_chunk()
         series.flush()
-
-        # This is required for creating the grid
-        if is_envelope:
-            array = array.astype(np.complex128)
-        else:
-            array = array.astype(np.float64)
 
         # Extract the required parameters to set the grid
         grid_offset = m.get_attribute("gridGlobalOffset")
@@ -122,22 +106,16 @@ class FromOpenPMDProfile(FromArrayProfile):
             axes_order = axes_order[::-1]
             array = np.swapaxes(array, idx_offset, 2)
 
-        # Read angular frequency
-        if is_envelope:
-            if omega0 is not None:
-                omg0 = omega0
-            else:
-                try:
-                    omg0 = m.get_attribute("angularFrequency")
-                except io.ErrorNoSuchAttribute:
-                    raise ValueError(
-                        "Angular frequency not found. Please provide the value.\
-                            If you are using Wake-T, please store the field as a"
-                    )
-        else:  # If electric field is provided, convert it to envelope
-            assert omega0 is None
-            temp_grid = create_grid(array, axes, dim, is_envelope=False)
-            grid, omg0 = field_to_envelope(temp_grid, dim)
+        # This block determines is the field is stored as envelope or not.
+        try:
+            # If field is stored as envelope, 
+            # it must contain the angular frequency as attribute.
+            omg0 = m.get_attribute("angularFrequency")
+        except io.ErrorNoSuchAttribute:
+            # If no angular frequency present, 
+            # then it must be the full electric field.
+            grid = create_grid(array, axes, dim, is_envelope=False)
+            grid, omg0 = field_to_envelope(grid, dim)
 
         wavelength = 2 * np.pi * c / omg0
 
