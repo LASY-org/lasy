93e5d4a74b025bce80a6c7b8d3e6c776a58da6e8
Maxence Thevenet <maxence.thevenet@desy.de>
2025-04-28 19:01:26 +0200

    fix

--- lasy/utils/fft.py
+++ lasy/utils/fft.py
@@ -64,19 +64,18 @@ def fft(which, arr_in, axes_in, from_domain):
         print("fft of size 1: do nothing")
         return arr_in, axes_in
     # Build output axes data
-    npoints = [i.size for i in axes_in]
     if transverse:
         # List of 2 elements for 2 transverse directions, (x, y) or (kx, ky)
         axes_out = [
-            np.fft.fftfreq(npoints[0], axes_in[0][1] - axes_in[0][0]),
-            np.fft.fftfreq(npoints[1], axes_in[1][1] - axes_in[1][0]),
+            np.fft.fftfreq(axes_in[0].size, axes_in[0][1] - axes_in[0][0]),
+            np.fft.fftfreq(axes_in[1].size, axes_in[1][1] - axes_in[1][0]),
         ]
         if from_domain == "real":
             axes_out[0] *= 2 * np.pi
             axes_out[1] *= 2 * np.pi
     else:
         # 1d array for longitudinal direction, t or omega
-        axes_out = np.fft.fftfreq(npoints[0], axes_in[1] - axes_in[0])
+        axes_out = np.fft.fftfreq(axes_in.size, axes_in[1] - axes_in[0])
         if from_domain == "real":
             axes_out *= 2 * np.pi
     if shift_after:
