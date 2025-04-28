5a8695778e4c75425d839b70e351c7862592a698
Cristina Mariani <166020667+CriMariani@users.noreply.github.com>
2025-04-28 15:58:49 +0200

    Update thick_lens.py

--- lasy/optical_elements/thick_lens.py
+++ lasy/optical_elements/thick_lens.py
@@ -17,7 +17,7 @@ class ThickLens(OpticalElement):
     d : float
         Thickness of the lens
     n_func : function
-        Function that returns the refractive index given the wavelength in microns, taken from the website https://refractiveindex.info .
+        Function that returns the refractive index given the wavelength in microns, taken from the website "https://refractiveindex.info".
         e.g. for Fused Silica: 
         nFS = lambda x: (1+0.6961663/(1-(0.0684043/x)**2)+0.4079426/(1-(0.1162414/x)**2)+0.8974794/(1-(9.896161/x)**2))**.5 
     """
