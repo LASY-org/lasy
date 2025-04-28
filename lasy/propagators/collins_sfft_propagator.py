68013fafe8094169bdef6e58e490a54d1cd0792e
Rob Shalloo <rob.shalloo@gmail.com>
2025-04-28 16:01:02 +0200

    fix docstrings

--- lasy/propagators/collins_sfft_propagator.py
+++ lasy/propagators/collins_sfft_propagator.py
@@ -2,9 +2,10 @@ from .single_fft_propagator import SingleFFTPropagator
 
 
 class CollinsSFFTPropagator(SingleFFTPropagator):
+    r"""
+    Single FFT propagator using the Collins approximation.
     """
-    An implementation of the single FFT propagator using the Collins approximation.
-    """
+
     def __init__(self):
         super().__init__()
         print("empty init for CollinsSFFTPropagator")
--- lasy/propagators/propagator.py
+++ lasy/propagators/propagator.py
@@ -1,5 +1,5 @@
 class Propagator(object):
-    """
+    r"""
     Base class for all propagators.
     """
 
@@ -8,9 +8,21 @@ class Propagator(object):
         return
 
     def update(self, dim=None, omega0=None):
+        """
+        Update the propagator parameters.
+        Parameters
+        ----------
+        dim : str
+            The dimension of the laser grid. Can be 'rt' or 'xyt'.
+        omega0 : float
+            The central frequency of the laser.
+        """
         self.dim = dim
         self.omega0 = omega0
         return
 
     def propagate(self, grid, distance=None, abcd=None):
+        """
+        Propagate the input grid using the specified method.
+        """
         return
