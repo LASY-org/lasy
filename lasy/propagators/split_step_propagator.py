0821cf11caea6389949bf191e0f34a2fa5466534
eichnert <timo.eichner@desy.de>
2025-04-28 15:59:59 +0200

    Implemented rough outline of split step propagator

--- lasy/propagators/split_step_propagator.py
+++ lasy/propagators/split_step_propagator.py
@@ -2,13 +2,33 @@ from .propagator import Propagator
 
 
 class SplitStepPropagator(Propagator):
-    def __init__(self):
+    def __init__(self, propagators):
+        """
+        Initialize the SplitStepPropagator.
+        
+        Parameters
+        ----------
+        propagators : list
+            List of propagators to be used in the split-step method.
+        """
         super().__init__()
-        print("empty init for SplitStepPropagator")
+        self.propagators = propagators
 
-    def update(self):
-        print("empty update for SplitStepPropagator")
-        pass
+    def propagate(self, grid, distance, nsteps=1):
+        """
+        Propagate the input grid using a split-step method.
 
-    def propagate(self, distance, grid_in, grid_out=None):
-        return grid_in
+        Parameters
+        ----------
+        grid : Grid
+            Input grid to be propagated.
+        distance : float
+            Distance over withh to propagate the field.
+        nsteps : int, optional
+            Number of steps to take during the propagation, by default 1
+        """
+        step_distance = distance / nsteps
+
+        for _ in range(nsteps):
+            for step_propagator in self.propagators:
+                step_propagator.propagate(grid=grid, distance=step_distance)
