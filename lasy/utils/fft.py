b0386fed1a864ea4eec2c7950d84d931b03f59d9
Maxence Thevenet <maxence.thevenet@desy.de>
2025-04-28 17:00:18 +0200

    closer to our use cases

--- lasy/utils/fft.py
+++ lasy/utils/fft.py
@@ -2,7 +2,7 @@ import numpy as np
 from numpy.fft import fft, ifft, fft2, fftshift, ifft2, ifftshift, fftfreq
 
 
-def fft(arr_in, axes_in, which, inverse=False, shift_before=True, shift_after=False):
+def fft(which, arr_in, axes_in, from_domain):
     """
     Perform FFT on a 3D array
 
@@ -19,8 +19,9 @@ def fft(arr_in, axes_in, which, inverse=False, shift_before=True, shift_after=Fa
         List of axes along which the FFT is to be performed:
         2 elements for which="transverse", 1 for which="longitudinal"
 
-    inverse : boolean
-        If true, perform ifft. Otherwise, use fft
+    from_domain : string
+        "real" of the FFT is done from real domain (x,y) or (t) to frequency domain (kx, ky) or (omega)
+        "frequency" for the opposite way
 
     shift_before : boolean
         Whether to perform fftshift on data before doing FFT.
@@ -37,25 +38,53 @@ def fft(arr_in, axes_in, which, inverse=False, shift_before=True, shift_after=Fa
         if which="transverse", 2 1d arrays for the transverse transformed axes
         if which="longitudinal", 1 1d array for the longitudinal transformed axis
     """
+
+    # Checks, read parameters and set defaults
     assert which in ["transverse", "longitudinal"]
+    assert from_domain in ["real", "frequency"]
     transverse = which == "transverse"
+    ax = (0, 1) if transverse else 2
+
+    # Here we set our conventions:
+    # - From real space to frequency space, we use ifft and
+    #   an fftshift is performed on the input data.
+    # - From frequency space to real space, we use fft and
+    #   an fftshift is performed on the output data.
+    if from_domain == "real":
+        shift_before = True
+        shift_after = False
+        inverse = True
+    else:
+        shift_before = False
+        shift_after = True
+        inverse = False
+
+    # Build output axes data
+    axes_out = axes_in.copy()
     npoints = [i.size for i in axes_in]
     dx = [i[1] - i[0] for i in axes_in]
-    ax = (0, 1) if transverse else 2
+    if transverse:
+        # List of 2 elements for 2 transverse directions, (x, y) or (kx, ky)
+        axes_out[0] = np.fft.fftfreq(npoints[0], dx[0])
+        axes_out[1] = np.fft.fftfreq(npoints[1], dx[1])
+        if from_domain == "real":
+            axes_out[0] *= 2 * np.pi
+            axes_out[1] *= 2 * np.pi
+    else:
+        # list of 1 element for longitudinal direction, t or omega
+        axes_out[0] = 2 * np.pi * np.fft.fftfreq(npoints[0], dx[0])
+        if from_domain == "real":
+            axes_out[0] *= 2 * np.pi
+
+    # Perform fftshift of input data if required. Then transform.
     if shift_before:
         if inverse:
-            arr = ifftshift(arr_in, axes=ax) if inverse else 
+            arr = ifftshift(arr_in, axes=ax)
         else:
             arr = fftshift(arr_in, axes=ax)
         arr_out = ifft2(arr, axes=ax) if transverse else ifft(arr, axes=ax)
     else:
         arr_out = fft2(arr, axes=ax) if transverse else fft(arr, axes=ax)
-    axes_out = axes_in.copy()
-    if transverse:
-        axes_out[0] = 2 * np.pi * np.fft.fftfreq(npoints[0], dx[0])
-        axes_out[1] = 2 * np.pi * np.fft.fftfreq(npoints[1], dx[1])
-    else:
-        axes_out[0] = 2 * np.pi * np.fft.fftfreq(npoints[0], dx[0])
     if shift_after:
         arr_out = ifftshift(arr, axes=ax) if inverse else fftshift(arr, axes=ax)
         if transverse:
