0d9030f21ab3cb024e4737c9f54eba8212e8724a
“Emily <emily.archer@desy.de>
2025-04-28 17:05:00 +0200

    em-archer

--- lasy/propagators/collins_sfft_propagator.py
+++ lasy/propagators/collins_sfft_propagator.py
@@ -1,6 +1,6 @@
 from .single_fft_propagator import SingleFFTPropagator
 
-from laser_utils import get_w0
+from lasy.utils.laser_utils import get_w0
 c = 2.998e8
 
 
@@ -9,7 +9,7 @@ class CollinsSFFTPropagator(SingleFFTPropagator):
         super().__init__()
         #self.update()
 
-        abcd = self.abcd
+        self.abcd = np.array([[1, 0],[0, 1]])
 
         return
 
@@ -26,7 +26,7 @@ class CollinsSFFTPropagator(SingleFFTPropagator):
         return self.abcd
 
 
-    def add_output_grid(self, grid_in, N_points, magnification=None, f0=None):
+    def add_output_grid(self, grid, N_points=100, magnification=1.0, f0=None):
         """
         Function to calculate the output grids 
         for a focusing / defocusing beam
@@ -49,24 +49,26 @@ class CollinsSFFTPropagator(SingleFFTPropagator):
     
         """        
         
-        # AT THE MOMENT THIS ASSUMES SYMMETRIC XY GRIDS
-
-        axes = grid_in.axes # xyt
+        # AT THE MOMENT THIS ASSUMES SYMMETRIC CARTESIAN GRIDS
+        axes = grid.axes
+        x = axes[0]
+        y = axes[1]
+        dx = x[1] - x[0]
+        dy = y[1] - y[0]
         
-        
-        self.w0 = self.laser.get_w0(self.grid, self.dim)
+        w0 = get_w0(grid, self.dim)
+        f0 = self.f0
         lambda_0 = 2. * np.pi * c / self.omega_0
         
-        N_points = 100
-        padFactor = int(np.ceil(magnification * (2. * w_0 / f_0) * L0_width / lambda_0 / N_points)) // 2 * 2 + 1 # Must be odd
+        padFactor = int(np.ceil(magnification * (2. * w0 / f0) * L0_width / lambda_0 / N_points)) // 2 * 2 + 1 # Must be odd
         print("Padding factor: ", padFactor)
         
         print("Number of spatial/transverse gridpoints: %0.0f" % (N_points))
-        r0_step = np.abs(np.max(grid_in[0])-np.min(grid_in[0])) / (N_points*padFactor)  # Note: D gridpoints means D-1 intervals
+        r0_step = np.abs(np.max(x)-np.min(x)) / (N_points*padFactor)  # Note: D gridpoints means D-1 intervals
         
-        x = fftshift(fftfreq(N_points*padFactor, r0_step) * lambda_0 * f_0) / padFactor
+        x = fftshift(fftfreq(N_points*padFactor, r0_step) * lambda_0 * f0) / padFactor
         y = fftshift(
-            fftfreq(N_points*padFactor, r0_step) * lambda_0 * f_0
+            fftfreq(N_points*padFactor, r0_step) * lambda_0 * f0
         ) / padFactor
         
         # Simulation output meshgrid
@@ -82,16 +84,10 @@ class CollinsSFFTPropagator(SingleFFTPropagator):
                       np.shape(R)[0]//2+W//2],
                       [np.shape(R)[1]//2-W//2,
                       np.shape(R)[1]//2+W//2]]) # Indexing of the focal region of interest
-        x_grid = x[region_idx[0,0]:region_idx[0,1]]
-        y_grid = y[region_idx[1,0]:region_idx[1,1]]
-        dx = np.abs(x_grid[1] - x_grid[0]) # Calculate resolutions
-        dy = np.abs(y_grid[1] - y_grid[0])
-
-        self.grid_in = R
-        return R, x_grid, y_grid
+        return [x,y], region_idx
     
 
-    def propagate(self, distance, grid_in=None, grid_out=None, abcd=None):
+    def propagate(self, grid, grid_out=None, distance=None, abcd=None):
         """
         Function to calculate an output field from 
         input field and optical ray matrix of the system
@@ -114,53 +110,86 @@ class CollinsSFFTPropagator(SingleFFTPropagator):
             The wavelength of the electric field
     
         """
-        # ASSUMES INPUT GRID IS MONOCHROMATIC MESHGRID OF R_in
-        
-        self.update()
+        axes = grid.axes
+        omega0 = self.omega0
 
+        # Get the spectral field and axes from the input grid
+        spectral_field, spectral_axes = grid.get_spectral_field()
+        
         if grid_out = None:
-            grid_out = self.grid_out
+            axes_out, region_idx = add_output_grid(grid) # Call routine to determine output grid
         else:
-            pass
+            grid_out = self.grid_out # Use user-specified grid
             
-        if abcd = None:
+        if abcd = None: # Update ABCD matrix if passed as variable
             abcd = self.abcd
         else:
             pass
 
         try:
-            A = matrix[0][0]
-            B = matrix[0][1]
-            C = matrix[1][0]
-            D = matrix[1][1]
+            A = abcd[0][0]
+            B = abcd[0][1]
+            C = abcd[1][0]
+            D = abcd[1][1]
         except:
             print("Missing the ray matrix for the optical system.")
+            
 
-        profile_in = self.profile_in
+        if self.dim == "rt":
+            print("Collins SFFT propagator in rt")
+            profile_out = None
 
-        padNumberx = int(np.shape(grid_out)[0]/np.shape(grid_in)[0])//2 * np.shape(grid_in)[0]
-        padNumbery = int(np.shape(grid_out)[1]/np.shape(grid_in)[1])//2 * np.shape(grid_in)[1]
+        elif self.dim == "xyt":
+            print("Collins SFFT propagator in xyt")
+            axes = grid.axes
+            x0 = axes[0]
+            y0 = axes[1]
+            dx0 = x0[1] - x0[0]
+            dy0 = y0[1] - y0[0]
+
+            x = axes_out[0]
+            y = axes_out[1]
+            dx = x[1] - x[0]
+            dy = y[1] - y[0]
+
+            X0, Y0, OM = np.meshgrid(y0[1], x0[0], spectral_axes + omega0)
+            X, Y, OM = np.meshgrid(y[1],  x[0],  spectral_axes + omega0)
+            K = OM / c
+            WAVELENGTH = 2 * np.pi * c / OM
+            
+            R0 = np.sqrt(X0**2 + Y0**2)
+            R = np.sqrt(X**2 + Y**2)
+            
+            profile_in = spectral_field
     
-        propagator = np.exp(1j * omega_0 / (2 * c) * (A / B) * grid_in**2)
+            padNumberx = int(np.shape(axes_out)[0]/np.shape(axes)[0])//2 * np.shape(axes)[0]
+            padNumbery = int(np.shape(axes_out)[1]/np.shape(axes)[1])//2 * np.shape(axes)[1]
     
-        profile_in = np.pad(profile_in, [(padNumberx, padNumbery), (padNumberx, padNumbery)], mode='constant')
-        propagator = np.pad(propagator, [(padNumberx, padNumbery), (padNumberx, padNumbery)], mode='constant')
-        print("Shape of padded profile: ",np.shape(profile_in))
-        
-        profile_out = fftshift(
-            ifft2(
-                ifftshift(profile_in * propagator, axes=(0, 1)),
+            propagator = np.exp(1j * OM / (2 * c) * (A / B) * R0**2)
+            
+            profile = np.pad(profile, [(padNumberx, padNumbery), (padNumberx, padNumbery)], mode='constant')
+            propagator = np.pad(propagator, [(padNumberx, padNumbery), (padNumberx, padNumbery)], mode='constant')
+            print("Shape of padded profile: ",np.shape(profile))
+            
+            profile_out = fftshift(
+                ifft2(
+                    ifftshift(profile * propagator, axes=(0, 1)),
+                    axes=(0, 1),
+                ),
                 axes=(0, 1),
-            ),
-            axes=(0, 1),
-        ) * np.sqrt(np.shape(grid_out)[0] * np.shape(grid_out)[1])
-        profile_out = (
-            profile_out
-            * np.exp(1j * omega_0 / (2 * c) * (D / B) * grid_out**2)
-            * omega_0
-            / (2j * np.pi * c * B)
-            / np.abs(omega_0 / (2j * np.pi * c * B))
-        )
+            ) * np.sqrt(np.shape(R)[0] * np.shape(R)[1])
+            profile_out = (
+                profile_out
+                * np.exp(1j * OM / (2 * c) * (D / B) * R**2)
+                * omega_0
+                / (2j * np.pi * c * B)
+                / np.abs(OM / (2j * np.pi * c * B))
+            )
+    
+            profile_out = profile_out[region_idx[0,0]:region_idx[0,1],region_idx[1,0]:region_idx[1,1]]  # Select ROI
+            x_grid = x[region_idx[0,0]:region_idx[0,1]]
+            y_grid = y[region_idx[1,0]:region_idx[1,1]]
+            
+            # Update grid here to be the new output grid and spectral field to be new spectral field
         
-        profile_out = profile_out[region_idx[0,0]:region_idx[0,1],region_idx[1,0]:region_idx[1,1]]  # Select ROI
         return profile_out
