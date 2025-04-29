1483b5f75e1c045646bd84aa83fc7a8653532fad
eichnert <timo.eichner@desy.de>
2025-04-29 11:18:13 +0200

    fix git issue

--- lasy/propagators/angular_spectrum_dfft_propagator.py
+++ lasy/propagators/angular_spectrum_dfft_propagator.py
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 from .propagator import Propagator
 from lasy.utils.fft_wrapper import fft
-=======
-import numpy as np
+
 from scipy.constants import c
-
-from lasy.utils.fourier import fft
->>>>>>> 6047ebc78956349d6fd87041bb3c4649f789fd8f
-
-from .propagator import Propagator
+import numpy as np
 
 
 class AngularSpectrumDFFTPropagator(Propagator):
@@ -16,24 +10,25 @@ class AngularSpectrumDFFTPropagator(Propagator):
     Angular spectrum dual FFT propagator.
     """
 
-<<<<<<< HEAD
     def __init__(self, omega0, n=1.):
 
-=======
-    def __init__(self, n=1.0):
->>>>>>> 6047ebc78956349d6fd87041bb3c4649f789fd8f
         super().__init__()
         self.n = n  # refractive index
         self.omega0 = omega0
     def propagate(self, distance, grid, dim):
-        if dim == "rt":
+        
+        if dim == 'rt':
             print("'rt' geometry not yet supported by AngularSpectrumPropagator")
 
-        if dim == "xyt":
+        if dim == 'xyt':
             # Get the spectral field in the spatial domain
             field, omega = grid.get_spectral_field()
 <<<<<<< HEAD
+<<<<<<< HEAD
 
+=======
+            
+>>>>>>> 8294da3 ()
             omega += self.omega0
             kz = omega/c
 
@@ -45,6 +40,7 @@ class AngularSpectrumDFFTPropagator(Propagator):
 
             kx = 2*np.pi*axes_freq[0]
             ky = 2*np.pi*axes_freq[1]
+<<<<<<< HEAD
 
 =======
 
@@ -62,9 +58,12 @@ class AngularSpectrumDFFTPropagator(Propagator):
             ky = 2 * np.pi * axes_freq[1]
 
 >>>>>>> 6047ebc78956349d6fd87041bb3c4649f789fd8f
+=======
+            
+>>>>>>> 8294da3 ()
             # Calculate the refractive index if it is a function of wavelength
             if type(self.n) not in [int, float, np.ndarray]:
-                wavelength = 2 * np.pi * c / omega
+                wavelength = 2*np.pi*c/omega
                 n = self.n(wavelength)
                 n0 = self.n(2*np.pi*c/self.omega0)
             else:
@@ -72,7 +71,6 @@ class AngularSpectrumDFFTPropagator(Propagator):
                 n0 = self.n
 
             # Calculate the phase shift in k-space
-<<<<<<< HEAD
             phase = (distance * n * (kz[None, None, :]**2 - kx[:, None, None]**2 - ky[None, :, None]**2) ** 0.5)
 
             # compensate group delay to keep pulse centered in grid
@@ -90,32 +88,15 @@ class AngularSpectrumDFFTPropagator(Propagator):
             gd = np.interp(self.omega0, omega_sorted, gd)
 
             phase = phase - gd*(omega-self.omega0)[None, None, :]
-=======
-            phase = (
-                distance
-                * n
-                * (kz[:, :, None] ** 2 - kx[None, :, :] ** 2 - ky[:, None, :] ** 2)
-                ** 0.5
-            )
->>>>>>> 6047ebc78956349d6fd87041bb3c4649f789fd8f
 
             # Apply the phase shift to the field in k-space
             field_kspace *= np.exp(1j * phase)
 
             # Transform back to the spatial domain
-<<<<<<< HEAD
             field, _ = fft(arr_in=field_kspace,
                                   which="transverse",
                                   axes_in=(kx/(2*np.pi), ky/(2*np.pi)),
                                   from_domain="real",)
-=======
-            field_kspace, _ = fft(
-                arr_in=field_kspace,
-                which="transverse",
-                axes_in=(kx / (2 * np.pi), ky / (2 * np.pi)),
-                from_domain="frequency",
-            )
->>>>>>> 6047ebc78956349d6fd87041bb3c4649f789fd8f
 
 
             grid.set_spectral_field(field)
--- tests/test_angular_spectral_propagator.py
+++ tests/test_angular_spectral_propagator.py
@@ -1,37 +1,23 @@
-import numpy as np
-
 from lasy.laser import Laser
 from lasy.profiles import GaussianProfile
+from lasy.utils.laser_utils import get_w0, get_duration
 from lasy.propagators import AngularSpectrumDFFTPropagator
-<<<<<<< HEAD
 import numpy as np
 from scipy.constants import c
-=======
-from lasy.utils.laser_utils import get_duration, get_w0
->>>>>>> 6047ebc78956349d6fd87041bb3c4649f789fd8f
 
 
 def make_laser():
-    profile = GaussianProfile(
-        wavelength=800e-9,
-        pol=(1, 0),
-        laser_energy=1,
-        tau=30e-15 / np.sqrt(2 * np.log(2)),
-        w0=100e-6,
-        t_peak=0,
-    )
+    profile = GaussianProfile(wavelength=800e-9,
+                              pol=(1, 0),
+                              laser_energy=1,
+                              tau=30e-15/np.sqrt(2*np.log(2)),
+                              w0=100e-6,
+                              t_peak=0)
 
-<<<<<<< HEAD
     dim = 'xyt'
     hi = (2e-3, 2e-3, 500e-15)
     lo = (-2e-3, -2e-3, -500e-15)
     npoints = (100, 100, 100)
-=======
-    dim = "xyt"
-    hi = (5e-3, 5e-3, 300e-15)
-    lo = (-5e-3, -5e-3, -300e-15)
-    npoints = (100, 100, 201)
->>>>>>> 6047ebc78956349d6fd87041bb3c4649f789fd8f
 
     laser = Laser(dim=dim, hi=hi, lo=lo, npoints=npoints, profile=profile)
 
@@ -39,11 +25,7 @@ def make_laser():
 
 
 def test_spatial_propagation():
-<<<<<<< HEAD
     prop = AngularSpectrumDFFTPropagator(omega0=2*np.pi*c/800e-9, n=1.)
-=======
-    prop = AngularSpectrumDFFTPropagator(n=1.0)
->>>>>>> 6047ebc78956349d6fd87041bb3c4649f789fd8f
 
     z_pos = np.linspace(-5e-3, 50e-3, 10)
     waists_propagated = []
@@ -54,14 +36,8 @@ def test_spatial_propagation():
         waist = get_w0(grid=laser.grid, dim=laser.dim)
         waists_propagated.append(waist)
 
-<<<<<<< HEAD
     zR = np.pi*laser.profile.w0**2/(laser.profile.lambda0)
     waists_analytical = laser.profile.w0 * np.sqrt(1 + (z_pos/zR)**2)
-=======
-    zR = np.pi * laser.profile.w0**2 / laser.profile.laser_wavelength
-
-    waists_analytical = laser.profile.w0 * np.sqrt(1 + (z_pos / zR) ** 2)
->>>>>>> 6047ebc78956349d6fd87041bb3c4649f789fd8f
 
     assert np.allclose(waists_propagated, waists_analytical, rtol=1e-5, atol=1e-6)
 
@@ -70,7 +46,6 @@ def n_fusedsilica(wavelength):
     """
     Sellmeier equation for fused silica
     """
-<<<<<<< HEAD
     x = wavelength * 1e6
     return (1+0.6961663/(1-(0.0684043/x)**2)+0.4079426/(1-(0.1162414/x)**2)+0.8974794/(1-(9.896161/x)**2))**.5
 
@@ -78,18 +53,6 @@ def n_fusedsilica(wavelength):
 def test_temporal_propagation():
 
     prop = AngularSpectrumDFFTPropagator(omega0=2*np.pi*c/800e-9, n=n_fusedsilica)
-=======
-    return (
-        1
-        + 0.6961663 / (1 - (0.0684043 / (wavelength * 1e-6)) ** 2)
-        + 0.4079426 / (1 - (0.1162414 / (wavelength * 1e-6)) ** 2)
-        + 0.8974794 / (1 - (9.896161 / (wavelength * 1e-6)) ** 2)
-    ) ** 0.5
-
-
-def test_temporal_propagation():
-    prop = AngularSpectrumDFFTPropagator(n=n_fusedsilica)
->>>>>>> 6047ebc78956349d6fd87041bb3c4649f789fd8f
 
     z_pos = np.linspace(0, 10e-3, 10)
     durations_propagated = []
@@ -101,9 +64,7 @@ def test_temporal_propagation():
         durations_propagated.append(duration)
 
     gdd = z_pos * 36.163e-27
-    tau_initial = laser.profile.tau * np.sqrt(2 * np.log(2))
-    duration_analytical = tau_initial * np.sqrt(
-        1 + (4 * np.log(2) * gdd / tau_initial**2) ** 2
-    )
+    tau_initial = laser.profile.tau*np.sqrt(2*np.log(2))
+    duration_analytical = tau_initial * np.sqrt(1 + (4*np.log(2)*gdd/tau_initial**2)**2)
 
     assert np.allclose(durations_propagated, duration_analytical, rtol=1e-5, atol=1e-15)
