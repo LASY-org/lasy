c91838b50cf7804ec48fc96ef7a41cbcc586f29b
eichnert <timo.eichner@desy.de>
2025-04-30 12:58:30 +0200

    added class documentation to split step propagator

--- lasy/propagators/split_step_propagator.py
+++ lasy/propagators/split_step_propagator.py
@@ -2,15 +2,17 @@ from .propagator import Propagator
 
 
 class SplitStepPropagator(Propagator):
-    def __init__(self, propagators):
-        """
-        Initialize the SplitStepPropagator.
+    """Class that represents a split step propagator.
 
-        Parameters
+    The propagator takes a list of propagator or nonlinear steppers as inputs 
+    and propagates an input grid by iterating through each of these sub-steps.
+
+    Parameters
         ----------
         propagators : list
-            List of propagators to be used in the split-step method.
-        """
+            List of propagators to be used as the sub-steps of the propagation.
+    """
+    def __init__(self, propagators):
         super().__init__()
         self.propagators = propagators
 
