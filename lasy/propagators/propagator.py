fccdc9d163f8cdb4ab86f7ed8192274eb405c65b
Maxence Thevenet <maxence.thevenet@desy.de>
2025-05-07 16:00:48 +0200

    implement dummy update properly

--- lasy/propagators/propagator.py
+++ lasy/propagators/propagator.py
@@ -13,6 +13,31 @@ class Propagator(ABC):
         pass
 
     @abstractmethod
+    def update(self, dim, omega0):
+        """
+        Update the propagator.
+
+        Typically called at the beginning of the propagate function, to update the propagator itself if required.
+        If the update is costly, some machinery should be in place to do it only if required.
+        Some propagators need to be updated if the distance to propagate changes.
+        Others only need to be updated if the grid properties change, etc.
+
+        Parameters
+        ----------
+        dim : string
+            Dimensionality of the array. Options are:
+
+            - ``'xyt'``: The laser pulse is represented on a 3D Cartesian grid.
+            - ``'rt'`` : The laser pulse is represented on a 2D cylindrical grid.
+
+        omega0 : float (in rad.s^-1)
+            The main frequency :math:`\omega_0`, which is defined by the laser
+            wavelength :math:`\lambda_0`, as :math:`\omega_0 = 2\pi c/\lambda_0`.
+        """
+        self.dim = dim
+        self.omega0 = omega0
+
+    @abstractmethod
     def propagate(self, distance, grid_in, dim, omega0, grid_out=None):
         r"""
         Propagate field in the grid along axis z, for a certain distance.
