0781bc9545ac761195d037e35484159f74c7a846
huixingjian <xingjian.hui@desy.de>
2025-06-24 14:31:07 +0200

    fix typo on np.copy

--- lasy/utils/fft_wrapper.py
+++ lasy/utils/fft_wrapper.py
@@ -66,7 +66,7 @@ def fft(which, arr_in, axes_in, from_domain):
             xfftshift = scc.fft.fftshift
 
         # Do the FFT
-        arr = scc.copy(arr_in)
+        arr = np.copy(arr_in)
         if shift_before:
             arr = xfftshift(arr, axes=(0, 1))
         arr_out = xfft(arr, axes=(0, 1))
