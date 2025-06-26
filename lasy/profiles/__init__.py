25843d396edd5746dbf189e418b335e0e5caff11
EliasMelch <emelche2@u.rochester.edu>
2025-06-26 10:38:31 -0400

    Added paraxial flying focus classes and tests.

--- lasy/profiles/__init__.py
+++ lasy/profiles/__init__.py
@@ -5,6 +5,13 @@ from .from_openpmd_profile import FromOpenPMDProfile
 from .gaussian_profile import GaussianProfile
 from .profile import Profile
 from .speckle_profile import SpeckleProfile
+from .flying_focus_profiles import (
+    FlyingFocusGaussianProfile,
+    FlyingFocusHGProfile,
+    FlyingFocusLGProfile,
+    FlyingFocusGaussianProfile2D,
+    FlyingFocusHGProfile2D
+)
 
 __all__ = [
     "Profile",
@@ -14,4 +21,9 @@ __all__ = [
     "FromOpenPMDProfile",
     "FromInsightFile",
     "SpeckleProfile",
+    "FlyingFocusGaussianProfile",
+    "FlyingFocusHGProfile",
+    "FlyingFocusLGProfile",
+    "FlyingFocusGaussianProfile2D",
+    "FlyingFocusHGProfile2D",
 ]
