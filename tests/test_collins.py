56e6c0f1205ef2f6d7b665eecde546911893c5ce
em-archer <emily.archer@desy.de>
2025-07-03 11:32:01 +0200

    Fixed tests
    Axial ranges needed to be set to be consistent with NF/FF approximations in Collins integral

--- tests/test_collins.py
+++ tests/test_collins.py
@@ -7,54 +7,65 @@ from lasy.propagators import ABCD, CollinsDFFTPropagator, CollinsSFFTPropagator
 from lasy.utils.laser_utils import get_w0
 
 
-def make_laser():
+def make_laserFF():
     profile = GaussianProfile(
         wavelength=800e-9,
         pol=(1, 0),
         laser_energy=1,
         tau=30e-15 / np.sqrt(2 * np.log(2)),
-        w0=500e-6,
+        w0=5e-3,
         t_peak=0,
     )
 
     dim = "xyt"
-    hi = (2e-3, 2e-3, 50e-15)
-    lo = (-2e-3, -2e-3, -50e-15)
-    npoints = (49, 49, 100)
+    hi = ( 20e-3,  20e-3,  50e-15)
+    lo = (-20e-3, -20e-3, -50e-15)
+    npoints = (499, 499, 1)
 
     laser = Laser(dim=dim, hi=hi, lo=lo, npoints=npoints, profile=profile)
 
     return laser
 
 
-def test_spatial_propagation_SFFT():
-    """Verify that the waist of Gaussian beam evolves as expected."""
-    laser = make_laser()
-    prop = CollinsSFFTPropagator(
-        dim=laser.dim,
-        omega0=2 * np.pi * c / 800e-9,
+def make_laserNF():
+    profile = GaussianProfile(
+        wavelength=800e-9,
+        pol=(1, 0),
+        laser_energy=1,
+        tau=30e-15 / np.sqrt(2 * np.log(2)),
+        w0=5e-6,
+        t_peak=0,
     )
 
-    focal_length = 1.0
+    dim = "xyt"
+    hi = ( 20e-6,  20e-6,  50e-15)
+    lo = (-20e-6, -20e-6, -50e-15)
+    npoints = (499, 499, 1)
 
-    zR = (
-        focal_length**2 * laser.profile.lambda0 / (np.pi * laser.profile.w0**2)
-    )  # Estimated Rayleigh range
-    w0 = (
-        laser.profile.lambda0 * focal_length / (np.pi * laser.profile.w0)
-    )  # Estimated focal spot-size
-    print(zR, w0)
+    laser = Laser(dim=dim, hi=hi, lo=lo, npoints=npoints, profile=profile)
 
-    z_pos = (
-        np.linspace(-10.0 * zR, 10.0 * zR, 5) + focal_length
-    )  # Absolute position from lens
+    return laser
+    
+
+def test_spatial_propagation_SFFT():
+    """Verify that the waist of Gaussian beam evolves as expected."""
+    laser = make_laserFF()
+    prop = CollinsSFFTPropagator(
+        dim=laser.dim, omega0=2 * np.pi * c / 800e-9, 
+    )
+    
+    focal_length = 100e-3
+    zR = focal_length**2 * laser.profile.lambda0 / (np.pi * laser.profile.w0**2) # Estimated Rayleigh range
+    w0 = laser.profile.lambda0 * focal_length / (np.pi * laser.profile.w0) # Estimated focal spot-size
+    
+    z_pos = np.linspace(-5.0 * zR, 5.0 * zR, 10) + focal_length # Absolute position from lens
     waists_propagated = []
-
-    i = 0
+    
+    i=0
     abcd = ABCD()
     abcd.add_lens(focal_length)
     for z in z_pos:
-        laser = make_laser()
+        laser = make_laserFF() # Propagate from input plane each time
         if i == 0:
             abcd.add_vacuum(z)
             prop.propagate(laser.grid, abcd)
@@ -65,43 +76,47 @@ def test_spatial_propagation_SFFT():
 
         waist = get_w0(grid=laser.grid, dim=laser.dim)
         waists_propagated.append(waist)
+        i+=1
 
-        laser.show()
-        i += 1
+    waists_analytical = w0 * np.sqrt(1 + (np.abs(z_pos-focal_length) / zR) ** 2)
 
-    waists_analytical = w0 * np.sqrt(1 + ((z_pos - focal_length) / zR) ** 2)
-    print(waists_propagated, waists_analytical, laser.profile.w0)
-
+    debug=False
+    if debug:
+        print('\t'.join(['{:.3e}'.format(x) for x in waists_propagated]))
+        print('\t'.join(['{:.3e}'.format(x) for x in waists_analytical]))
+        print(np.isclose(waists_propagated, waists_analytical, rtol=1e-5, atol=1e-6))
+    
     assert np.allclose(waists_propagated, waists_analytical, rtol=1e-5, atol=1e-6)
 
 
 def test_spatial_propagation_DFFT():
     """Verify that the waist of Gaussian beam evolves as expected."""
-    laser = make_laser()
+    laser = make_laserNF()
     prop = CollinsDFFTPropagator(
-        dim=laser.dim,
-        omega0=2 * np.pi * c / 800e-9,
+        dim=laser.dim, omega0=2 * np.pi * c / 800e-9, 
     )
-
-    z_pos = np.linspace(-50e-3, 50e-3, 5)
+    
+    z_pos = np.linspace(-1e-9, 1e-9, 10)
     waists_propagated = []
 
     for z in z_pos:
+        laser = make_laserNF()
+        
         abcd = ABCD()
         abcd.add_vacuum(z)
-
-        laser = make_laser()
-        laser.add_propagator(prop)
-
-        prop.propagate(laser.grid, abcd, grid_out=laser.grid)
+        prop.propagate(
+            laser.grid, abcd
+        ) 
 
         waist = get_w0(grid=laser.grid, dim=laser.dim)
         waists_propagated.append(waist)
 
-        laser.show()
-
     zR = np.pi * laser.profile.w0**2 / (laser.profile.lambda0)
-    waists_analytical = laser.profile.w0 * np.sqrt(1 + (z_pos / zR) ** 2)
-    print(waists_propagated - waists_analytical, laser.profile.w0)
+    waists_analytical = laser.profile.w0 * np.sqrt(1 + np.abs(z_pos / zR) ** 2)
+    debug=False
+    if debug:
+        print('\t'.join(['{:.3e}'.format(x) for x in waists_propagated]))
+        print('\t'.join(['{:.3e}'.format(x) for x in waists_analytical]))
+        print(np.isclose(waists_propagated, waists_analytical, rtol=1e-5, atol=1e-6))
 
     assert np.allclose(waists_propagated, waists_analytical, rtol=1e-5, atol=1e-6)
