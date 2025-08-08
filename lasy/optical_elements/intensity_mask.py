bd26a23625858ddce304bcee42c4cb2a76106557
CoraBr <cora.braun@desy.de>
2025-08-08 14:35:56 +0200

    Update intensity_mask.py
    Added the possibility to create rectangular apertures or masks

--- lasy/optical_elements/intensity_mask.py
+++ lasy/optical_elements/intensity_mask.py
@@ -12,23 +12,35 @@ class IntensityMask(OpticalElement):
 
     Parameters
     ----------
-    R : float (in meter)
-        The radius of the mask
+    R : float (in meter) or tuple (floats)
+        The radius of the mask for round masks or half-width and half-height for rectangular masks as tuple.
+        If the shape is rectangular and only one number is given, a quadratic shape is assumed. 
     center: tuple (floats)
         Center of the mask. Default is (0,0)
     mask_type: string
         Should be 'aperture' (default, allows light inside) or 'hole' (allows light outside).
+    shape: string
+        Should be 'round' (default, for round apertures) or 'rectangular' (for rectangular apertures).
 
     """
 
-    def __init__(self, R, center=(0, 0), mask_type="aperture"):
+    def __init__(self, R, center=(0, 0), mask_type="aperture",shape="round"):
         assert mask_type in ["aperture", "hole"], (
             "mask_type must be 'aperture' or 'hole'"
         )
+        assert shape in ["round", "rectangular"], (
+            "shape must be 'round' or 'rectangular'"
+        )
+        
         self.R = R
         self.center = center
         self.mask_type = mask_type
-
+        self.shape = shape
+        if shape==round:
+            assert R.type==float (
+            "Radius cannot be a tuple'"
+        )
+        
     def amplitude_multiplier(self, x, y, omega):
         """
         Return the amplitude multiplier.
@@ -45,10 +57,23 @@ class IntensityMask(OpticalElement):
             Contains the value of the multiplier at the specified points.
             This array has the same shape as the array omega.
         """
-        r_squared = (x - self.center[0]) ** 2 + (y - self.center[1]) ** 2
-        mask = r_squared <= self.R**2  # True inside, False outside
+        if shape=='round':
+            r_squared = (x - self.center[0]) ** 2 + (y - self.center[1]) ** 2
+            mask = r_squared <= self.R**2  # True inside, False outside
+
+        if shape=='rectangular':
+            if R.type==float
+                halfwidth=R
+                halfheight=R
+            if R.type==tuple:
+                halfwidth=R[0]
+                halfheight=R[1]
+            mask = ((x-self.center[0]) <= halfwidth) and ((x-self.center[0]) >= -halfwidth) and ((y-self.center[y]) <= halfheight) and ((y-self.center[y]) >= halfheight)) # True inside, False outside
 
         if self.mask_type == "aperture":
             return mask.astype(float)  # 1 inside, 0 outside
         else:  # "hole"
             return (~mask).astype(float)  # 0 inside, 1 outside
+
+            
+
