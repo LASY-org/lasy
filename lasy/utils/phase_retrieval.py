ad5869540e880393e1b9b755fb6ffeb9545c8d81
em-archer <emily.archer@desy.de>
2025-09-24 15:44:44 +0200

    Updating phase retrieval and adding tutorial
    Currently not working due to use of propagators

--- lasy/utils/phase_retrieval.py
+++ lasy/utils/phase_retrieval.py
@@ -1,96 +1,128 @@
 import copy
 
 import numpy as np
+import matplotlib.pyplot as plt
+from lasy.optical_elements.parabolic_mirror import ParabolicMirror
+from lasy.utils.mode_decomposition import (
+    estimate_best_HG_waist,
+    hermite_gauss_composition,
+    hermite_gauss_decomposition,
+)
 
-
-def gerchberg_saxton_algo(
-    laserPos1,
-    laserPos2,
-    dz,
-    condition="max_iterations",
-    max_iterations=10,
-    amplitude_error=1.0,
-    debug=False,
-):
+class GerchbergSaxton():
     """
-    Implement the Gerchberg-Saxton Algorithm.
-
-    Given two laser profiles and a distance betweent them, calculate
-    the spatial phase profile of the laser in both planes using the
-    Gerchberg-Saxton algorithm. Returns the phase of the laser in both
-    planes.
-
-    Parameters
-    ----------
-    laserPos1, laserPos2 : instance of Laser
-
-    dz : float (meters)
-        Distance between the two laser pulses
-
-    condition : string
-        The condition on which to stop the iterative loop.
-        Options are: 'max_iterations', stopping after a fixed number of
-        iterations or 'amplitude_error', waiting for the residual of
-        the e-field amplitude from laserPos1 to fall below 'amplitude_error'
-
-    max_iterations : int
-        Maximum number of iterations to perform
-
-    amplitude_error : float
-        Residual value for amplitude given as a fraction of the maximum
-        initial amplitude value for laserPos1.
-
-    debug : boolean (default: False)
-        if True, the error at each iteration is printed to standard output
-
-    Returns
-    -------
-    phase1, phase2 : ndarray of floats (rad)
-        Phase profiles of the laser pulse at the locations where
-        laserPos1 and laserPos2 are defined.
+    An implementation of the Gerchberg Saxton Algorithm with Modal Decomposition (GSA-MD)
+    as described by I. Moulanier et al., Jour. Opt. Soc. Am. B 40, 9 (2023). DOI 10.1364/JOSAB.489884
+    
     """
-    laser1 = copy.deepcopy(laserPos1)
-    laser2 = copy.deepcopy(laserPos2)
-    amp1 = np.abs(laser1.grid.get_temporal_field())
-    amp1_summed = np.sum(amp1)
-    amp2 = np.abs(laser2.grid.get_temporal_field())
-    phase1 = np.zeros_like(amp1)
+    
+    def __init__(self, lasers, positions, m_max=10, n_max=10, max_iter=100, initial_phase=None, verbose=False):
+        """
+        Parameters
+        ----------
 
-    if condition == "max_iterations":
+    
 
-        def breakout(i):
-            return i < max_iterations
+        """
+        self.lasers = lasers
+        self.positions = positions
+        self.m_max = m_max
+        self.n_max = n_max
+        self.max_iter = max_iter
+        self.intial_phase = self.initial_phase
+        self.verbose = verbose    
 
-        cond = 0
-    elif condition == "amplitude_error":
+    def initialise_lasers(self):
+        
+        self.laser1 = copy.deepcopy(self.lasers[0])
+        self.laser2 = copy.deepcopy(self.lasers[1])
+        
+        self.amp1 = np.abs(self.laser1.grid.get_temporal_field())
+        self.amp2 = np.abs(self.laser2.grid.get_temporal_field())
+        
+        self.x = self.laser2.grid.axes[0]
+        self.y = self.laser2.grid.axes[1]
 
-        def breakout(amp):
-            return amp / amp1_summed > amplitude_error
+        self.dz = self.positions[1] - self.positions[0]
+        self.initialise_spotsizes()
+        
+        
+        
+    def initialise_spotsizes(self,spotsizes=None):
+        """
+        Initialise the spot size of the modes at focus in each transverse direction
+        
+        Parameters
+        ----------
+        spotSizes: tuple of floats (meters)
+            The size of the mode along the x and y axes at focus. spotSize[0] corresponds to 
+            the y-axis while spotSize[1] corresponds to the x-axis.  If none is provided then
+            the spot sizes will be initialised based on a D4sigma fit to the
+            focus.
+            
+        """
+        
+        if spotsizes is None:
+            w0x, w0y = estimate_best_HG_waist(
+                x, y, self.amp2, self.laser2.profile.lambda0
+            ) # Estimate spot size in the focal plane
+            spotsizes = (w0x, w0y)
 
-        cond = 9e30
+        self.spotsizes = spotsizes
 
-    i = 0
-    while breakout(cond):
-        laser1.grid.set_temporal_field(amp1 * np.exp(1j * phase1))
-        laser1.propagate(dz, verbose=False)
+    
+    def initialise_modes(self):
+        
 
-        phase2 = np.angle(laser1.grid.get_temporal_field())
-        laser2.grid.set_temporal_field(amp2 * np.exp(1j * phase2))
-        laser2.propagate(-dz, verbose=False)
-
-        field2 = laser2.grid.get_temporal_field()
-        phase1 = np.angle(field2)
-        amp1_calc = np.abs(field2)
-        amp_error_summed = np.sum(np.abs(amp1_calc) - amp1)
-        if debug:
-            i += 1
-            print(
-                "Iteration %i : Amplitude Error (summed) = %.2e"
-                % (i, amp_error_summed / amp1_summed)
-            )
-        if condition == "max_iterations":
-            cond += 1
+        # Initialise random phase if no known phase is passed
+        if self.intial_phase is None:
+            self.phase1 = np.pi * np.random.uniform(-1, 1, np.shape(self.amp1)) # Initial guess of phase
         else:
-            cond = amp_error_summed
+            self.phase1 = self.intial_phase
 
-    return phase1, phase2, amp_error_summed
+        # Construct Initial Guess of Field
+        self.laser1.grid.set_temporal_field(self.amp1*np.exp(1j*self.phase1))
+        
+        # Find estimate of the decomposition of the initial electric field
+        self.modes = hermite_gauss_decomposition(self.laser1, self.spotsizes[0], self.spotsizes[1], self.m_max, self.n_max)
+
+    
+    def retrieve_phase(self):
+
+    
+        def breakout(i):
+            return i < self.max_iter
+        cond = 0
+
+        errors = []
+    
+        i = 0
+        while breakout(cond):
+            laser1.grid.set_temporal_field(amp1 * np.exp(1j * phase1))
+
+            
+            # Calculate the decomposition and waist of the laser pulse
+            modes = hermite_gauss_decomposition(laser_raw, w0x, w0y, m_max, n_max)
+            laser1.apply_optics(ParabolicMirror(dz))
+            laser1.propagate(dz)
+    
+            phase2 = np.angle(laser1.grid.get_temporal_field())
+            laser2.grid.set_temporal_field(amp2 * np.exp(1j * phase2))
+            laser2.propagate(-dz)
+            laser2.apply_optics(ParabolicMirror(-dz))
+    
+            phase1 = np.angle(laser2.grid.get_temporal_field())
+            
+            amp_error_summed = np.sum(np.abs(np.abs(laser2.grid.get_temporal_field())-amp1)) / np.sum(amp1)
+            
+            i += 1
+            cond += 1
+            if self.verbose:
+                print(
+                    "Iteration %i : Amplitude Error (summed) = %.2e"
+                    % (i, amp_error_summed)
+                )
+
+            errors.append(amp_error_summed)
+    
+        return phase1, phase2, np.array(errors)
