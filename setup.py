3e19b6048c2a3a69df13f4385fe78eb721c487cf
em-archer <emily.archer@desy.de>
2026-01-08 19:31:50 +0100

    Fix for PR #410
    Modified setup.py to install .json files (e.g. refractive_index_db.json)

--- setup.py
+++ setup.py
@@ -17,7 +17,11 @@ with open("./tests/requirements.txt") as f:
 setup(
     name="lasy",
     version=lasy.__version__,
-    packages=find_packages("."),
+    packages=find_packages("."),    
+    include_package_data=True,
+    package_data={
+        "lasy": ["**/*.json"],
+    },
     description="LAser pulse manipulation made eaSY",
     long_description=long_description,
     long_description_content_type="text/markdown",
