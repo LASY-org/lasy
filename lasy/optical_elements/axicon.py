94f5ad2e72f74ff19f4e9ff4e83d1abcdc71de75
Emily Archer <emily.archer@desy.de>
2026-09-14 15:38:17 +0200

    Update lasy/optical_elements/axicon.py
    Co-authored-by: Maxence Thévenet <maxence.thevenet@desy.de>

--- lasy/optical_elements/axicon.py
+++ lasy/optical_elements/axicon.py
@@ -57,7 +57,7 @@ class Axicon(OpticalElement):
         The axicon type to use, ``reflective`` or ``diffractive``.
         For a refractive axicon, use ``reflective``, the profiles are identical in this implementation.
     lambda0 : float (in m, optional, default=0.8e-6)
-        If using axicon_type=``diffractive``, the design (angular) wavelength for which
+        If using axicon_type=``diffractive``, the design wavelength for which
         the diffractive axicon's physical groove profile was fabricated should
         be specified.
     """
